b727c0cb283a858143963b8816ced07a95918824
Avi Bahra <Avi.Bahra@ecmwf.int>
2013-11-30 11:47:28 +0000

    Fix build

--- ANattr/test/TestVariable.cpp
+++ ANattr/test/TestVariable.cpp
@@ -13,6 +13,7 @@
 // Description :
 /////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
 
+#include <iostream>
 #include <boost/test/unit_test.hpp>
 
 #include "Variable.hpp"
