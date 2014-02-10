b42c0d9c26a96879c37a52ba4a3d94caa533b065
Avi Bahra <Avi.Bahra@ecmwf.int>
2014-02-10 13:20:59 +0000

    remove files with no code/commented out

--- ACore/src/File_r.hpp
+++ ACore/src/File_r.hpp
@@ -18,43 +18,14 @@
 
 #include <boost/noncopyable.hpp>
 #include <string>
-
-#if defined(AIX_GCC)
-#include <stdio.h>
-#else
 #include <fstream>
-#endif
 
 namespace ecf {
 
-#if defined(AIX_GCC)
-
 class File_r : private boost::noncopyable {
 public:
-
-   /// For read only, to work around AIX gcc problems
    File_r(const std::string& file_name);
    ~File_r();
-   bool ok() const;
-   bool good() const;
-   void getline(std::string& line);
-
-   const std::string& file_name() const { return file_name_; }
-
-private:
-   std::string file_name_;
-
-   FILE* fp_;
-   bool good_;
-};
-
-#else
-
-class File_r : private boost::noncopyable {
-public:
-
-   File_r(const std::string& file_name) : file_name_(file_name), fp_(file_name.c_str(), std::ios_base::in) {}
-   ~File_r() { fp_.close(); }
 
    bool ok() const { return (fp_) ? true : false; }
    bool good() const { return fp_.good(); }
@@ -65,8 +36,6 @@ private:
    std::string file_name_;
    std::ifstream fp_;
 };
-
-#endif
 
 }
 
