d92bb20a0d512c9d6d0d6ac742cea4b80aa5e5ac
Avi Bahra <Avi.Bahra@ecmwf.int>
2014-02-25 17:24:58 +0000

    Fix setup.py for AIX, this still requires a custome hack for AIX. i.e need to set env LDSHARED.

--- Pyext/setup.py
+++ Pyext/setup.py
@@ -1,7 +1,39 @@
 from distutils.core import setup, Extension
 import os
-
+import sys
+import glob
+from numpy.f2py.crackfortran import endifpattern
+# ========================================================================
+# Usage:
+#   python setup.py build_ext
+#   python setup.py install
+# Test:
+#   python setup.py install --home=~
+#
 # See: http://docs.python.org/2/distutils/apiref.html?highlight=extension#distutils.core.Extension
+#
+# ========================================================================
+# AIX: On AIX we need custom compile and link flags:
+# The final link line is wrong: xlC_r xlc_r  , 
+#                              /usr/local/lpp/vacpp11109/usr/vacpp/bin/.orig/xlC_r: 1501-228 (W) input file xlc_r not found
+#           
+# Removing the duplicated xlc_r, We then get a little further,
+#         ld: 0711-317 ERROR: Undefined symbol: .main
+#      Using -bnoquiet
+#        .main                     [12]    ER PR crt0_64.s(/lib/crt0_64.o)
+#                                          00000070 .text    R_RBR    [34]    .__start
+# This is because it is missing -G (generate dynamic library)
+# When we manually add -G, sucessfull link(ecflow.so)
+#
+#      Both of these can be fixed, by overriding the Link line with:
+#      export LDSHARED="xlC_r -G -Wl,-bI:/usr/local/apps/python/2.7.2-01/lib/python2.7/config/python.exp"
+#
+# To find out what python is going to use:
+#         python -c "from distutils import sysconfig; print sysconfig.get_config_vars('LDSHARED')[0]"
+# On AIX, it does not use this  value ? 
+#
+#      See python issue 18235  _sysconfigdata.py wrong on AIX installations
+# ==========================================================================
 
 def get_ecflow_version():
     "This will extract ecFlow version from the source code."
@@ -12,8 +44,7 @@ def get_ecflow_version():
     "set( ECFLOW_MINOR    \"2\" )"
     "set( ${PROJECT_NAME}_VERSION_STR  \"${ECFLOW_RELEASE}.${ECFLOW_MAJOR}.${ECFLOW_MINOR}\" )"
     "will return a list of form `[4,0,2]`" 
-    work_space=os.getenv("WK")
-    file = work_space + "/VERSION.cmake"
+    file = "../VERSION.cmake"
     ecflow_version = []
     if os.path.exists(file):
         version_cpp = open(file,'r')
@@ -41,63 +72,54 @@ print ecflow_build_dir_version;
 print ecflow_version;
 
 #===============================================================================================
-# define the name of the extension to use
-extension_name    = 'ecflow'
-extension_version = ecflow_version
- 
 # define the directories to search for include files
 # to get this to work, you will need to include the path
-# to your boost installation.  
-work_space=os.getenv("WK") 
+# to your boost installation and  ecflow includes
 boost_root=os.getenv("BOOST_ROOT") 
-boost_python_dir = boost_root + "/stage/lib"
+include_dirs = [ "../ACore/src", 
+                 "../ANattr/src", 
+                 "../ANode/src",
+                 "../AParser/src",
+                 "../Base/src",
+                 "../Base/src/cts",
+                 "../Base/src/stc",
+                 "../CSim/src", 
+                 "../Client/src",
+                 "../Pyext/src",
+                 boost_root,
+               ]
 
-# Now all the ecflow includes
-core_include     = work_space + "/ACore/src";
-anattr_include   = work_space + "/ANattr/src";
-anode_include    = work_space + "/ANode/src";
-parser_include   = work_space + "/AParser/src";
-base_include     = work_space + "/Base/src";
-base_cts_include = work_space + "/Base/src/cts";
-base_stc_include = work_space + "/Base/src/stc";
-csim_include     = work_space + "/CSim/src";
-client_include   = work_space + "/Client/src";
-pyext_include    = work_space + "/Pyext/src";
-include_dirs = [ core_include, 
-                 anattr_include, 
-                 anode_include,
-                 parser_include,
-                 base_include,
-                 base_cts_include,
-                 base_stc_include,
-                 csim_include, 
-                 client_include,
-                 pyext_include,
-                 boost_python_dir ]
+# define the library directories to include any extra libraries that may be needed.
+# Give preference to release libs   
+boost_lib_dir = boost_root + "/stage/lib/"
+if os.path.isdir("../ecbuild/release"):
+    library_dirs = ['../ecbuild/release/ACore/',
+                    '../ecbuild/release/ANattr/',
+                    '../ecbuild/release/ANode/',
+                    '../ecbuild/release/AParser/', 
+                    '../ecbuild/release/Base/', 
+                    '../ecbuild/release/CSim/', 
+                    '../ecbuild/release/Client/', 
+                    boost_lib_dir  
+                   ]
+else:
+    library_dirs = ['../ecbuild/debug/ACore/',
+                    '../ecbuild/debug/ANattr/',
+                    '../ecbuild/debug/ANode/',
+                    '../ecbuild/debug/AParser/', 
+                    '../ecbuild/debug/Base/', 
+                    '../ecbuild/debug/CSim/', 
+                    '../ecbuild/debug/Client/', 
+                    boost_lib_dir  
+                   ]
 
-# define the library directories to include any extra libraries that may be needed.   
-boost_python_dir = boost_root + "/stage/lib/"
-libcore_dir = work_space + '/ecbuild/debug/ACore/'
-libnodeattr_dir = work_space + '/ecbuild/debug/ANattr/'
-libnode_dir = work_space + '/ecbuild/debug/ANode/'
-libparser_dir = work_space +  '/ecbuild/debug/AParser/'
-libbase_dir = work_space +  '/ecbuild/debug/Base/'
-libsim_dir = work_space + '/ecbuild/debug/CSim/'
-libclient_dir = work_space + '/ecbuild/debug/Client/'
-library_dirs = [  libcore_dir,libnodeattr_dir,libnode_dir,libparser_dir, libbase_dir, libsim_dir, libclient_dir, boost_python_dir  ]
 
+# We link to boost python statically, hence no need to setup runtime_libs
 # define any runtime dependencies. This will setup RPATH
-runtime_libs = [ boost_python_dir ]
+# runtime_libs = [ boost_python_dir ]
 
 # define the libraries to link with this includes the boost lib
-libcore =  'core'
-libnodeattr = 'nodeattr'
-libnode = 'node'
-libparser =  'libparser'
-libbase =   'base'
-libsim =   'libsimu'
-libclient = 'libclient'
-libraries = [ libcore , libnodeattr, libnode, libparser, libbase, libsim, libclient,
+libraries = [ 'core' , 'nodeattr', 'node', 'libparser', 'base', 'libsimu', 'libclient',
               'boost_system-mt',
               'boost_serialization-mt',
               'boost_filesystem-mt',
@@ -105,18 +127,34 @@ libraries = [ libcore , libnodeattr, libnode, libparser, libbase, libsim, libcli
               'boost_date_time-mt', 
               'boost_python-mt' ]
  
-# define the source files for the extension
-source_files = [ 'src/BoostPythonUtil.cpp', 
-                 'src/ClientDoc.cpp', 
-                 'src/DefsDoc.cpp', 
-                 'src/EcfExt.cpp', 
-                 'src/ExportClient.cpp', 
-                 'src/ExportCore.cpp',
-                 'src/ExportNode.cpp',
-                 'src/ExportNodeAttr.cpp',
-                 'src/ExportSuiteAndFamily.cpp',
-                 'src/ExportTask.cpp',
-                 'src/NodeAttrDoc.cpp' ]
+# extra compile flags needed for AIX only
+# Note setup.py will add -q64 -qcpluscmt -DNDEBUG  automatically
+# Note: two extra_compile_args, debug and release, use the debug for testing and faster compiles
+extra_compile_args = []
+extra_link_args = []
+if sys.platform.startswith("aix"):
+   #extra_compile_args =  [ '-qsuppress=1540-0198', '-O3', '-qstrict', '-qfuncsect', '-qeh', '-qrtti'  ]
+   extra_compile_args = [ '-qsuppress=1540-0198',  '-qNOOPTimize', '-qnoinline', '-qfullpath', '-qfuncsect', '-qeh', '-qrtti' ]
+   extra_link_args= [ '-lpthread', '-ldl', '-bbigtoc' ]  # '-G', '-noipath', 
  
+
 # create the extension and add it to the python distribution
-setup( name=extension_name, version=extension_version, ext_modules=[Extension( extension_name, source_files, include_dirs=include_dirs, library_dirs=library_dirs, runtime_library_dirs=runtime_libs, libraries=libraries )] )
+# The configuation below creates:
+#  lib/python2.7/site-packages/ecflow/ecflow.so
+#                                     __init__.py               
+setup( name='ecflow', 
+       version=ecflow_version, 
+       packages = [ 'ecflow' ],
+       package_dir={'ecflow': 'ecflow'},
+       ext_modules=[ Extension( 
+                              'ecflow.ecflow', 
+                              glob.glob(os.path.join('src', '*.cpp')) , 
+                              include_dirs=include_dirs, 
+                              library_dirs=library_dirs, 
+                              libraries=libraries,
+                              extra_compile_args=extra_compile_args,
+                              extra_link_args=extra_link_args,
+                              language='c++'
+                              )
+                    ],
+   )
