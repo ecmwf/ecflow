fba770746929e764535a9615483f6d7fd32d3715
Avi Bahra <Avi.Bahra@ecmwf.int>
2014-03-24 11:03:12 +0000

    added new flag to valgrind

--- build/val.sh
+++ build/val.sh
@@ -31,13 +31,13 @@ echo "valgrind: variant=$mode compiler=$compiler"
 # valgrind Server/bin/$compiler/$mode/ecflow_server --interval=3
 # Then restart this shell
 
-valgrind --leak-check=full --show-reachable=yes ACore/bin/$compiler/$mode/u_acore
-valgrind --leak-check=full --show-reachable=yes ANattr/bin/$compiler/$mode/u_anattr
-valgrind --leak-check=full --show-reachable=yes ANode/bin/$compiler/$mode/u_anode
-valgrind --leak-check=full --show-reachable=yes AParser/bin/$compiler/$mode/u_aparser
-valgrind --leak-check=full --show-reachable=yes Base/bin/$compiler/$mode/u_base
-valgrind --leak-check=full --show-reachable=yes Client/bin/$compiler/$mode/s_client
-valgrind --leak-check=full --show-reachable=yes Server/bin/$compiler/$mode/u_server
-valgrind --leak-check=full --show-reachable=yes --num-callers=30 Test/bin/$compiler/$mode/s_test
-valgrind --leak-check=full --show-reachable=yes --num-callers=30 Test/bin/$compiler/$mode/s_test_zombies
-valgrind --leak-check=full --show-reachable=yes CSim/bin/$compiler/$mode/c_csim
+valgrind --num-callers=24 --leak-check=full --show-reachable=yes ACore/bin/$compiler/$mode/u_acore
+valgrind --num-callers=24 --leak-check=full --show-reachable=yes ANattr/bin/$compiler/$mode/u_anattr
+valgrind --num-callers=24 --leak-check=full --show-reachable=yes ANode/bin/$compiler/$mode/u_anode
+valgrind --num-callers=24 --leak-check=full --show-reachable=yes AParser/bin/$compiler/$mode/u_aparser
+valgrind --num-callers=24 --leak-check=full --show-reachable=yes Base/bin/$compiler/$mode/u_base
+valgrind --num-callers=24 --leak-check=full --show-reachable=yes Client/bin/$compiler/$mode/s_client
+valgrind --num-callers=24 --leak-check=full --show-reachable=yes Server/bin/$compiler/$mode/u_server
+valgrind --num-callers=24 --leak-check=full --show-reachable=yes --num-callers=30 Test/bin/$compiler/$mode/s_test
+valgrind --num-callers=24 --leak-check=full --show-reachable=yes --num-callers=30 Test/bin/$compiler/$mode/s_test_zombies
+valgrind --num-callers=24 --leak-check=full --show-reachable=yes CSim/bin/$compiler/$mode/c_csim
