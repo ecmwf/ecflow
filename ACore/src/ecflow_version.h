ede73093fcd73415b38513d227c669ccf1d7af94
Avi Bahra <Avi.Bahra@ecmwf.int>
2014-04-07 14:24:15 +0100

    Update version number

--- ACore/src/ecflow_version.h
+++ ACore/src/ecflow_version.h
@@ -1,10 +1,10 @@
 #ifndef ecflow_version_config_h
 #define ecflow_version_config_h
  
-#define ECFLOW_VERSION   "4.0.2"
+#define ECFLOW_VERSION   "4.0.3"
 
 #define ECFLOW_RELEASE   "4"
 #define ECFLOW_MAJOR     "0"
-#define ECFLOW_MINOR     "2"
+#define ECFLOW_MINOR     "3"
 
 #endif 
