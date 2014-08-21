4471347324ceae8d4cf27a705d0b468f7b8cdaa5
Avi Bahra <Avi.Bahra@ecmwf.int>
2014-08-21 09:33:47 +0100

    debug

--- Pyext/test/ecflow_test_util.py
+++ Pyext/test/ecflow_test_util.py
@@ -176,10 +176,9 @@ class Server(object):
     
     def __exit__(self,exctype,value,tb):
         print "Server:__exit__: Kill the server, clean up log file, check pt files and lock files, ECF_HOME"
-        print "Server:__exit__(exctype,value,tb):";
-        print exctype
-        print value
-        print tb
+        print "exctype:"; print exctype
+        print "value:"; print value
+        print "tb:"; print tb
         if not debugging():
             print "   Terminate server"
             self.ci.terminate_server()  
