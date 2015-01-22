86bdbddc5a2b84f73960fbba1f4be344ec0f2fcb
Avi Bahra <Avi.Bahra@ecmwf.int>
2015-01-22 10:20:11 +0000

    Update version to be consistent for extraction

--- VERSION.cmake
+++ VERSION.cmake
@@ -3,6 +3,5 @@ set( ECFLOW_MAJOR    "0" )
 set( ECFLOW_MINOR    "7" )
 
 # use this form to be consistent with other packages, + allow standard extraction of version from scripts
+# could replaced "4.0.7" with "${ECFLOW_RELEASE}.${ECFLOW_MAJOR}.${ECFLOW_MINOR}"
 set( ${PROJECT_NAME}_VERSION_STR  "4.0.7" )
-
-#set( ${PROJECT_NAME}_VERSION_STR  "${ECFLOW_RELEASE}.${ECFLOW_MAJOR}.${ECFLOW_MINOR}" )
