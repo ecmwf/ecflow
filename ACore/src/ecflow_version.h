ca7d8e7953fcc798a015066b7a2d35a0be7d6bd6
Avi Bahra <Avi.Bahra@ecmwf.int>
2015-03-19 17:59:41 +0000

    update to 4.0.8 for next release

--- ACore/src/ecflow_version.h
+++ ACore/src/ecflow_version.h
@@ -1,10 +1,10 @@
 #ifndef ecflow_version_config_h
 #define ecflow_version_config_h
  
-#define ECFLOW_VERSION   "4.0.7"
+#define ECFLOW_VERSION   "4.0.8"
 
 #define ECFLOW_RELEASE   "4"
 #define ECFLOW_MAJOR     "0"
-#define ECFLOW_MINOR     "7"
+#define ECFLOW_MINOR     "8"
 
 #endif 
