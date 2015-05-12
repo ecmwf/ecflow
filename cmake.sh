e3be31987cd135926db3e31ce092d28a2f75baab
Avi Bahra <Avi.Bahra@ecmwf.int>
2015-05-12 09:38:17 +0100

    Update cmake doc

--- cmake.sh
+++ cmake.sh
@@ -1,5 +1,4 @@
 #!/bin/ksh
-
 # ====================================================================
 show_error_and_exit() {
    echo "cmake.sh expects at least one argument, note 'san' is short for thread sanitiser i.e"
@@ -28,6 +27,8 @@ set -u # fail when using an undefined variable
 set -x # echo script lines as they are executed
 
 # ====================================================================
+# To load module automatically requires Korn shell, system start scripts
+# auto adds ability to module load
 cmake_extra_options=""
 if [[ "$2" = clang ]] ; then
 	module unload gnu
@@ -40,16 +41,18 @@ if [[ "$3" = san ]] ; then
 	cmake_extra_options="$cmake_extra_options -DCMAKE_C_FLAGS=-fsanitize=thread"
 fi
 
-# ===================================================================
+# ====================================================================================
 cd $WK
 release=$(cat VERSION.cmake | grep 'set( ECFLOW_RELEASE' | awk '{print $3}'| sed 's/["]//g')
 major=$(cat VERSION.cmake   | grep 'set( ECFLOW_MAJOR'   | awk '{print $3}'| sed 's/["]//g')
 minor=$(cat VERSION.cmake   | grep 'set( ECFLOW_MINOR'   | awk '{print $3}'| sed 's/["]//g')
 
+# ====================================================================================
 rm -rf ecbuild/$1
 mkdir -p ecbuild/$1
 cd ecbuild/$1
-      
+    
+# ====================================================================================  
 cmake_build_type=
 if [[ $1 = debug ]] ; then
     cmake_build_type=Debug
@@ -57,7 +60,7 @@ else
     cmake_build_type=Release
 fi
 
-# ==========================================================================================================
+# ====================================================================================
 #
 # -DCMAKE_PYTHON_INSTALL_TYPE = [ local | setup ]
 # -DCMAKE_PYTHON_INSTALL_PREFIX should *only* used when using python setup.py (CMAKE_PYTHON_INSTALL_TYPE=setup)
@@ -82,7 +85,7 @@ ctest -R py_s
 ctest -R s_
 
 
-# ===============================================================
+# ============================================================================================
 # The process for thread checking very simple when using clang thread sanitiser
 #*) Load the module for Clang
 #module unload gnu
