a204602ffe44ddd20d5c3e36016c5286e7b78d68
Avi Bahra <Avi.Bahra@ecmwf.int>
2015-06-16 11:18:11 +0100

    Update 4.0.8rc1 --> 4.0.8

--- ACore/src/ecflow_version.h
+++ ACore/src/ecflow_version.h
@@ -1,10 +1,10 @@
 #ifndef ecflow_version_config_h
 #define ecflow_version_config_h
  
-#define ECFLOW_VERSION   "4.0.8rc1"
+#define ECFLOW_VERSION   "4.0.8"
 
 #define ECFLOW_RELEASE   "4"
 #define ECFLOW_MAJOR     "0"
-#define ECFLOW_MINOR     "8rc1"
+#define ECFLOW_MINOR     "8"
 
 #endif 
--- VERSION.cmake
+++ VERSION.cmake
@@ -1,7 +1,7 @@
 set( ECFLOW_RELEASE  "4" )
 set( ECFLOW_MAJOR    "0" )
-set( ECFLOW_MINOR    "8rc1" )
+set( ECFLOW_MINOR    "8" )
 
 # use this form to be consistent with other packages,+ allow standard extraction of version from scripts
 # could replaced "4.0.8" with "${ECFLOW_RELEASE}.${ECFLOW_MAJOR}.${ECFLOW_MINOR}"
-set( ${PROJECT_NAME}_VERSION_STR  "4.0.8rc1" )
+set( ${PROJECT_NAME}_VERSION_STR  "4.0.8" )
