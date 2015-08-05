5fb014b56e0a1dc9aabba2c2b48fc42507a6ab34
Avi Bahra <Avi.Bahra@ecmwf.int>
2015-08-05 09:20:04 +0100

    add some debug comments

--- Client/test/EcfPortLock.hpp
+++ Client/test/EcfPortLock.hpp
@@ -45,7 +45,7 @@ public:
    static void create(const std::string& the_port)
    {
       std::string the_file = port_file( the_port );
-   //   std::cout << "EcfPortLock::create " << the_file << "\n";
+      std::cout << "EcfPortLock::create " << the_file << "\n";
       std::string errorMsg;
       if (!ecf::File::create(the_file,"",errorMsg)) {
          std::stringstream sb;
@@ -57,7 +57,7 @@ public:
    static void remove(const std::string& the_port)
    {
       std::string the_file = port_file(the_port);
-   //   std::cout << "EcfPortLock::remove " << the_file << "\n";
+      std::cout << "EcfPortLock::remove " << the_file << "\n";
       boost::filesystem::remove(the_file);
    }
 
