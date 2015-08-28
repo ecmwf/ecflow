82bd06c8457afeff74f29f821ee54f50fed972d7
Avi Bahra <Avi.Bahra@ecmwf.int>
2015-08-28 15:41:10 +0100

    romev $WK

--- cmake.sh
+++ cmake.sh
@@ -96,7 +96,6 @@ fi
 
 # ====================================================================================
 # Use for local install
-cd $WK
 release=$(cat VERSION.cmake | grep 'set( ECFLOW_RELEASE' | awk '{print $3}'| sed 's/["]//g')
 major=$(cat VERSION.cmake   | grep 'set( ECFLOW_MAJOR'   | awk '{print $3}'| sed 's/["]//g')
 minor=$(cat VERSION.cmake   | grep 'set( ECFLOW_MINOR'   | awk '{print $3}'| sed 's/["]//g')
