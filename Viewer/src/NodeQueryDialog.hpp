18eac14859b39f55a38029d56b67bd6fb380093e
Sandor Kertesz <cgr@ecmwf.int>
2015-11-03 10:22:27 +0000

    ecFlowUI: implement search dialogue for nodes and attributes ECFLOW-423

--- Viewer/src/NodeQueryDialog.hpp
+++ Viewer/src/NodeQueryDialog.hpp
@@ -2,34 +2,14 @@
 #define NODEQUERYDIALOG_HPP_
 
 #include <QDialog>
-#include <QAbstractItemModel>
 
 #include "ServerFilter.hpp"
 
 #include "ui_NodeQueryDialog.h"
-#include "ui_NodeQuerySaveDialog.h"
 
-class NodeQuery;
-class NodeQueryEngine;
-class NodeQueryListModel;
-class NodeQueryModel;
-class QSortFilterProxyModel;
+class ServerFilter;
 
-class NodeQuerySaveDialog : public QDialog, protected Ui::NodeQuerySaveDialog
-{
-Q_OBJECT
-
-public:
-    explicit NodeQuerySaveDialog(QWidget *parent = 0);
-    ~NodeQuerySaveDialog() {};
-    QString name() const;
-
-public Q_SLOTS:
-	void accept();
-};
-
-
-class NodeQueryDialog : public QDialog, protected Ui::NodeQueryDialog, public ServerFilterObserver
+class NodeQueryDialog : public QDialog, protected Ui::NodeQueryDialog
 {
     Q_OBJECT
 
@@ -39,97 +19,17 @@ public:
 
     void setServerFilter(ServerFilter*);
 
-    void notifyServerFilterAdded(ServerItem*);
-    void notifyServerFilterRemoved(ServerItem*);
-    void notifyServerFilterChanged(ServerItem*);
-    void notifyServerFilterDelete();
-
 protected Q_SLOTS:
 	void accept();
     void reject();
-    void buildQueryString();
-	void slotTypeCbChanged();
-	void slotStateCbChanged();
-	void slotFlagCbChanged();
-	void slotSearchTermEdited(QString);
-	void slotRootNodeEdited(QString);
-	void slotExactMatch(bool);
-	void slotFind();
-	void check();
-	void slotStop();
-	void slotAddResult(QStringList);
-	void slotQueryStarted();
-	void slotQueryFinished();
-	void slotSaveQueryAs();
 
 protected:
 	void closeEvent(QCloseEvent * event);
 
 private:
-	void updateServers();
-	void initAttr();
-	void updateQuery();
     void readSettings();
     void writeSettings();
-
-	NodeQuery* query_;
-	NodeQueryEngine* engine_;
-	NodeQueryModel* model_;
-	QSortFilterProxyModel* sortModel_;
-
-	NodeQueryListModel* queryListModel_;
-	ServerFilter* serverFilter_;
 };
-
-class NodeQueryModel : public QAbstractItemModel
-{
- Q_OBJECT
-public:
-   	explicit NodeQueryModel(QObject *parent=0);
-   	~NodeQueryModel();
-
-   	int columnCount (const QModelIndex& parent = QModelIndex() ) const;
-   	int rowCount (const QModelIndex& parent = QModelIndex() ) const;
-
-   	QVariant data (const QModelIndex& , int role = Qt::DisplayRole ) const;
-   	QVariant headerData(int,Qt::Orientation,int role = Qt::DisplayRole ) const;
-
-   	QModelIndex index (int, int, const QModelIndex& parent = QModelIndex() ) const;
-   	QModelIndex parent (const QModelIndex & ) const;
-
-   	void clearData();
-
-public Q_SLOTS:
-	void appendRow(QStringList);
-
-protected:
-	QList<QStringList> data_;
-};
-
-
-class NodeQueryListModel : public QAbstractItemModel
-{
-public:
-   	explicit NodeQueryListModel(QObject *parent=0);
-   	~NodeQueryListModel();
-
-   	int columnCount (const QModelIndex& parent = QModelIndex() ) const;
-   	int rowCount (const QModelIndex& parent = QModelIndex() ) const;
-
-   	QVariant data (const QModelIndex& , int role = Qt::DisplayRole ) const;
-   	QVariant headerData(int,Qt::Orientation,int role = Qt::DisplayRole ) const;
-
-   	QModelIndex index (int, int, const QModelIndex& parent = QModelIndex() ) const;
-   	QModelIndex parent (const QModelIndex & ) const;
-
-   	void updateData();
-
-protected:
-	std::vector<NodeQuery*> data_;
-};
-
-
-
 
 
 #endif
---
+++ Viewer/src/NodeQueryWidget.hpp
@@ -0,0 +1,143 @@
+//============================================================================
+// Copyright 2014 ECMWF.
+// This software is licensed under the terms of the Apache Licence version 2.0
+// which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+// In applying this licence, ECMWF does not waive the privileges and immunities
+// granted to it by virtue of its status as an intergovernmental organisation
+// nor does it submit to any jurisdiction.
+//
+//============================================================================
+#ifndef VIEWER_SRC_NODEQUERYWIDGET_HPP_
+#define VIEWER_SRC_NODEQUERYWIDGET_HPP_
+
+#include <QAbstractItemModel>
+#include <QDialog>
+#include <QWidget>
+
+#include "ServerFilter.hpp"
+
+#include "ui_NodeQueryWidget.h"
+#include "ui_NodeQuerySaveDialog.h"
+
+class NodeQuery;
+class NodeQueryEngine;
+class NodeQueryListModel;
+class NodeQueryModel;
+class QSortFilterProxyModel;
+
+class NodeQuerySaveDialog : public QDialog, protected Ui::NodeQuerySaveDialog
+{
+Q_OBJECT
+
+public:
+    explicit NodeQuerySaveDialog(QWidget *parent = 0);
+    ~NodeQuerySaveDialog() {};
+    QString name() const;
+
+public Q_SLOTS:
+	void accept();
+};
+
+
+class NodeQueryWidget : public QWidget, protected Ui::NodeQueryWidget, public ServerFilterObserver
+{
+    Q_OBJECT
+
+public:
+    explicit NodeQueryWidget(QWidget *parent = 0);
+    ~NodeQueryWidget();
+
+    void setServerFilter(ServerFilter*);
+
+    void notifyServerFilterAdded(ServerItem*);
+    void notifyServerFilterRemoved(ServerItem*);
+    void notifyServerFilterChanged(ServerItem*);
+    void notifyServerFilterDelete();
+
+protected Q_SLOTS:
+	//void accept();
+    //void reject();
+    void buildQueryString();
+	void slotTypeCbChanged();
+	void slotStateCbChanged();
+	void slotFlagCbChanged();
+	void slotSearchTermEdited(QString);
+	void slotRootNodeEdited(QString);
+	void slotExactMatch(bool);
+	void slotFind();
+	void check();
+	void slotStop();
+	void slotAddResult(QStringList);
+	void slotQueryStarted();
+	void slotQueryFinished();
+	void slotSaveQueryAs();
+
+protected:
+	//void closeEvent(QCloseEvent * event);
+
+private:
+	void updateServers();
+	void initAttr();
+	void updateQuery();
+    //void readSettings();
+    //void writeSettings();
+
+	NodeQuery* query_;
+	NodeQueryEngine* engine_;
+	NodeQueryModel* model_;
+	QSortFilterProxyModel* sortModel_;
+
+	NodeQueryListModel* queryListModel_;
+	ServerFilter* serverFilter_;
+};
+
+class NodeQueryModel : public QAbstractItemModel
+{
+ Q_OBJECT
+public:
+   	explicit NodeQueryModel(QObject *parent=0);
+   	~NodeQueryModel();
+
+   	int columnCount (const QModelIndex& parent = QModelIndex() ) const;
+   	int rowCount (const QModelIndex& parent = QModelIndex() ) const;
+
+   	QVariant data (const QModelIndex& , int role = Qt::DisplayRole ) const;
+   	QVariant headerData(int,Qt::Orientation,int role = Qt::DisplayRole ) const;
+
+   	QModelIndex index (int, int, const QModelIndex& parent = QModelIndex() ) const;
+   	QModelIndex parent (const QModelIndex & ) const;
+
+   	void clearData();
+
+public Q_SLOTS:
+	void appendRow(QStringList);
+
+protected:
+	QList<QStringList> data_;
+};
+
+
+class NodeQueryListModel : public QAbstractItemModel
+{
+public:
+   	explicit NodeQueryListModel(QObject *parent=0);
+   	~NodeQueryListModel();
+
+   	int columnCount (const QModelIndex& parent = QModelIndex() ) const;
+   	int rowCount (const QModelIndex& parent = QModelIndex() ) const;
+
+   	QVariant data (const QModelIndex& , int role = Qt::DisplayRole ) const;
+   	QVariant headerData(int,Qt::Orientation,int role = Qt::DisplayRole ) const;
+
+   	QModelIndex index (int, int, const QModelIndex& parent = QModelIndex() ) const;
+   	QModelIndex parent (const QModelIndex & ) const;
+
+   	void updateData();
+
+protected:
+	std::vector<NodeQuery*> data_;
+};
+
+
+
+#endif /* VIEWER_SRC_NODEQUERYWIDGET_HPP_ */
