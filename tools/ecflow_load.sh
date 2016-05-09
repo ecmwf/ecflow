c18da7eb71308026a7a3bbf7840d796196e973db
Avi Bahra <Avi.Bahra@ecmwf.int>
2016-05-09 09:55:35 +0100

    Update license

--- tools/ecflow_load.sh
+++ tools/ecflow_load.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+## Copyright 2009-2016 ECMWF. 
+## This software is licensed under the terms of the Apache Licence version 2.0 
+## which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
+## In applying this licence, ECMWF does not waive the privileges and immunities 
+## granted to it by virtue of its status as an intergovernmental organisation 
+## nor does it submit to any jurisdiction. 
+# =====================================================================
+# Show server load
+# =====================================================================
 set -x
 ECF_PORT=$(($(id -u) + 1500))
 ECF_NODE=$(uname -n)
