4b1b132265f2334b6bffe63a964f4ad3825de29e
Avi Bahra <Avi.Bahra@ecmwf.int>
2016-05-11 14:57:40 +0100

    update ecbuild

--- cmake.sh
+++ cmake.sh
@@ -102,7 +102,7 @@ set -x # echo script lines as they are executed
 # To load module automatically requires Korn shell, system start scripts
 
 module load cmake/3.3.2
-module load ecbuild
+module load ecbuild/2.2.0
 cmake_extra_options=""
 if [[ "$clang_arg" = clang ]] ; then
 	module unload gnu
