6ed5f98fda2e660c5f898a0e594748353e7c5e36
Avi Bahra <Avi.Bahra@ecmwf.int>
2016-05-12 11:04:36 +0100

    Updated

--- Pyext/samples/check_modules.py
+++ Pyext/samples/check_modules.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2.7
-import argparse  
-import re     
+import argparse
+import re
 if __name__ == "__main__":
     DESC = """Will check module are correct:
               Usage:
@@ -13,23 +13,24 @@ if __name__ == "__main__":
     PARSER.add_argument('--f',help="Path to a file that is the output of module list")
     PARSER.add_argument('--remote_host',help="remote host")
     ARGS = PARSER.parse_args()
-    #print ARGS  
-    
+    #print ARGS
+
     delimter = "module"
     module_cmds = [delimter+e for e in ARGS.m.split(delimter) if e != ""]
     #print module_cmds
-    
+
+    module_actions = ("load","add","rm","unload","swap","switch")
     for mod in module_cmds:
         module_cmd = mod.split()
         #print ">>",module_cmd,"<<"
-        if len(module_cmd) < 3 and module_cmd[0] != "module": 
+        if len(module_cmd) < 3 and module_cmd[0] != "module":
             print "Error: Module command ",module_cmds[0]," is not correctly formed, first string must be module"
             exit(1)
-        if module_cmd[1] != "swap" and module_cmd[1] != "load" and module_cmd[1] != "unload" and module_cmd[1] != "switch": 
-            print "Error: Module command ",ARGS.m," is not correctly formed, second arg must be one of swap,load,unload,switch"
+        if not module_cmd[1] in module_actions:
+            print "Error: Module command ",module_cmd," is not correctly formed, second arg must be one of add,load,rm,unload,switch,swap"
             exit(1)
-    
-    # open file,for list of modules,  
+
+    # open file,for list of modules,
     #   Currently Loaded Modulefiles:
     #   1) version/3.2.10(default)             7) ecfs/2.2.1-rc2(new:prodn:default)
     #   2) verbose/true(default)               8) metview/4.6.4(default)
@@ -37,9 +38,9 @@ if __name__ == "__main__":
     #   4) gnu/4.8.1(default)                 10) fftw/3.3.4(default)
     #   5) python/2.7.8-01(default)           11) emos/437(default)
     #   6) sms/4.4.14(default)                12) ecmwf/1.0(default)
- 
+
     module_list = list()
-    module_list_file = open(ARGS.f) 
+    module_list_file = open(ARGS.f)
     try:
         for line in module_list_file:
             #print line
@@ -52,13 +53,13 @@ if __name__ == "__main__":
             for tok in tokens:
                 module_list.append(tok)
     finally:
-         module_list_file.close()  
+         module_list_file.close()
     #print module_list
-    
+
     for mod in module_cmds:
         module_cmd = mod.split()
         #print ">>",module_cmd,"<<"
-        if module_cmd[1] == "load":
+        if module_cmd[1] == "load" or module_cmd[1] == "add":
             if not any(module_cmd[2] in s for s in module_list):
                 if ARGS.remote_host =="sappa" or ARGS.remote_host =="sappb":
                     if module_cmd[2] == "git": continue  # ignore, uses system git
@@ -72,11 +73,11 @@ if __name__ == "__main__":
             if not any(module_cmd[-1] in s for s in module_list):
                 print "Error: package",module_cmd[-1],"not found in the module list"
                 exit(1)
-        if module_cmd[1] == "unload":
+        if module_cmd[1] == "unload" or module_cmd[1] == "rm":
            if any(module_cmd[2] in s for s in module_list):
                 print "Error:",mod,"found in the module list"
                 exit(1)
-
+                
 # 
 # module list > module_list_file 2>&1 && less module_list_file
 # FAIL:
