3f2de9d0d8447dbf71f4308181e94d2b51d96bd6
Avi Bahra <Avi.Bahra@ecmwf.int>
2016-06-22 10:58:58 +0100

    ECFLOW-684 change for rpm, allow daniel to know this is            not the final version

--- VERSION.cmake
+++ VERSION.cmake
@@ -1,6 +1,6 @@
 set( ECFLOW_RELEASE  "4" )
 set( ECFLOW_MAJOR    "1" )
-set( ECFLOW_MINOR    "1" )
+set( ECFLOW_MINOR    "1rc1" )
 
 # use this form to be consistent with other packages,+ allow standard extraction of version from scripts
 # could replaced "4.0.8" with "${ECFLOW_RELEASE}.${ECFLOW_MAJOR}.${ECFLOW_MINOR}"
