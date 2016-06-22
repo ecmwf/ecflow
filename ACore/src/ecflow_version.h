8fc77eac374388478099e412636f948a85260cf8
Avi Bahra <Avi.Bahra@ecmwf.int>
2016-06-22 14:03:44 +0100

    ECFLOW-684 update version for candidate rpm release

--- ACore/src/ecflow_version.h
+++ ACore/src/ecflow_version.h
@@ -1,7 +1,7 @@
 #ifndef ecflow_version_config_h
 #define ecflow_version_config_h
  
-#define ECFLOW_VERSION   "4.1.1"
+#define ECFLOW_VERSION   "4.1.1rc1"
 
 #define ECFLOW_RELEASE   "4"
 #define ECFLOW_MAJOR     "1"
