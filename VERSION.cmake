93cc1bc5e6cadfba64e401d3d3d66d9bea8fafc2
Avi Bahra <Avi.Bahra@ecmwf.int>
2016-06-22 11:08:29 +0100

    ECFLOW-684 update version number

--- VERSION.cmake
+++ VERSION.cmake
@@ -4,4 +4,4 @@ set( ECFLOW_MINOR    "1rc1" )
 
 # use this form to be consistent with other packages,+ allow standard extraction of version from scripts
 # could replaced "4.0.8" with "${ECFLOW_RELEASE}.${ECFLOW_MAJOR}.${ECFLOW_MINOR}"
-set( ${PROJECT_NAME}_VERSION_STR  "4.1.1" )
+set( ${PROJECT_NAME}_VERSION_STR  "4.1.1rc1" )
