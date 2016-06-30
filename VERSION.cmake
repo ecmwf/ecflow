ff16310de8e474b509d91b644ebfc0d29b7cb5dd
Avi Bahra <Avi.Bahra@ecmwf.int>
2016-06-30 10:01:21 +0100

    prepare for 4.2.0 as an external release

--- VERSION.cmake
+++ VERSION.cmake
@@ -1,7 +1,7 @@
 set( ECFLOW_RELEASE  "4" )
-set( ECFLOW_MAJOR    "1" )
-set( ECFLOW_MINOR    "1rc1" )
+set( ECFLOW_MAJOR    "2" )
+set( ECFLOW_MINOR    "0" )
 
 # use this form to be consistent with other packages,+ allow standard extraction of version from scripts
 # could replaced "4.0.8" with "${ECFLOW_RELEASE}.${ECFLOW_MAJOR}.${ECFLOW_MINOR}"
-set( ${PROJECT_NAME}_VERSION_STR  "4.1.1rc1" )
+set( ${PROJECT_NAME}_VERSION_STR  "4.2.0" )
