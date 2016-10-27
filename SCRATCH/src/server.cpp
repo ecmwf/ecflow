052269c1c3a449e53da62657717df8e65ef8048c
Avi Bahra <Avi.Bahra@ecmwf.int>
2016-10-27 11:01:36 +0100

    Update SCRATCH ssl example

--- SCRATCH/src/server.cpp
+++ SCRATCH/src/server.cpp
@@ -105,7 +105,7 @@ public:
     context_.set_password_callback(boost::bind(&server::get_password, this));
     context_.use_certificate_chain_file("server.crt");
     context_.use_private_key_file("server.key", boost::asio::ssl::context::pem);
-    context_.use_tmp_dh_file("dh512.pem");
+    context_.use_tmp_dh_file("dh1024.pem");
 
     session* new_session = new session(io_service_, context_);
     acceptor_.async_accept(new_session->socket(),
