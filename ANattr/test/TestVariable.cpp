26bf382863423835efa6465de4c7554823f098b2
Avi Bahra <Avi.Bahra@ecmwf.int>
2016-12-12 12:48:09 +0000

    ECFLOW-833

--- ANattr/test/TestVariable.cpp
+++ ANattr/test/TestVariable.cpp
@@ -14,6 +14,7 @@
 /////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
 
 #include <iostream>
+#include <stdio.h>
 #include <boost/test/unit_test.hpp>
 
 #include "Variable.hpp"
@@ -50,6 +51,55 @@ BOOST_AUTO_TEST_CASE( test_multi_line_variable_values )
       Variable var("name","val1\nxxx\nval2");
       std::string expected = "edit name 'val1\\nxxx\\nval2'";
       BOOST_CHECK_MESSAGE(var.toString() == expected,"expected " << expected << " but found " << var.toString());
+   }
+}
+
+BOOST_AUTO_TEST_CASE( test_variable_value )
+{
+   cout << "ANattr:: ...test_variable_value\n";
+
+   std::vector<std::string> values;
+   values.push_back("sdsd");
+   values.push_back("0fred0");
+   values.push_back("fted");
+   values.push_back("%value%");
+   values.push_back("a");
+   values.push_back("");
+   values.push_back("0");
+   values.push_back("00");
+   values.push_back("000");
+   values.push_back("0000");
+   values.push_back("0000000000000");
+   for(size_t i=0; i < values.size(); i++) {
+      Variable var("name","");
+      var.set_value(values[i]);
+      BOOST_CHECK_MESSAGE(var.value() == 0,"expected 0 but found " << var.value() << " for " << values[i]);
+   }
+
+   {
+      Variable var("name","0100");
+      BOOST_CHECK_MESSAGE(var.value() == 100,"expected 100 but found " << var.value());
+   }
+   {
+      Variable var("name","0001");
+      BOOST_CHECK_MESSAGE(var.value() == 1,"expected 1 but found " << var.value());
+   }
+   {
+      Variable var("name","2359");
+      BOOST_CHECK_MESSAGE(var.value() == 2359,"expected 2359 but found " << var.value());
+   }
+
+   // make sure time is convertible to an integer
+   char smstime[255];
+   for(int h = 0; h < 24; h++) {
+      for(int m=1; m < 60; m++ ) {
+         int output_written = sprintf(smstime,"%02d%02d", h,m);
+         BOOST_CHECK_MESSAGE(output_written == 4," expected size 4 but found " << output_written);
+         Variable var("name","");
+         var.set_value( smstime);
+         int value = atoi(smstime);
+         BOOST_CHECK_MESSAGE(var.value() == value,"expected " << value << " but found " << var.value());
+      }
    }
 }
 
