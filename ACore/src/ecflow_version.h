ffb9fb06b6b8952ded41c7971cc4e0eb0c9f8093
Avi Bahra <Avi.Bahra@ecmwf.int>
2017-02-27 09:43:21 +0000

    Update to version 5.0.0

--- ACore/src/ecflow_version.h
+++ ACore/src/ecflow_version.h
@@ -1,10 +1,10 @@
 #ifndef ecflow_version_config_h
 #define ecflow_version_config_h
  
-#define ECFLOW_VERSION   "4.6.0"
+#define ECFLOW_VERSION   "5.0.0"
 
-#define ECFLOW_RELEASE   "4"
-#define ECFLOW_MAJOR     "6"
+#define ECFLOW_RELEASE   "5"
+#define ECFLOW_MAJOR     "0"
 #define ECFLOW_MINOR     "0"
 
 #endif 
