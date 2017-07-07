1455ae511be6a4452b53b12c7cf656db9caeb595
Avi Bahra <Avi.Bahra@ecmwf.int>
2017-07-07 10:29:07 +0100

    ECFLOW-1027  refactor expression parsing to remove duplicate code

--- Base/src/cts/AlterCmd.cpp
+++ Base/src/cts/AlterCmd.cpp
@@ -993,43 +993,37 @@ void AlterCmd::createChange( Cmd_ptr& cmd, std::vector<std::string>& options, st
       break; }
 
 	case AlterCmd::TRIGGER: {
-		if (options.size() != 3) {
-			ss << "AlterCmd: change: expected four args : change trigger 'expression' <path_to_node>";
-			ss << " but found " << (options.size() + paths.size()) << " arguments. The trigger expression must be quoted\n";
-			ss << dump_args(options,paths) << "\n";
-			throw std::runtime_error( ss.str() );
-		}
-		name = options[2];
+	   if (options.size() != 3) {
+	      ss << "AlterCmd: change: expected four args : change trigger 'expression' <path_to_node>";
+	      ss << " but found " << (options.size() + paths.size()) << " arguments. The trigger expression must be quoted\n";
+	      ss << dump_args(options,paths) << "\n";
+	      throw std::runtime_error( ss.str() );
+	   }
+	   name = options[2];
 
-		// Parse the expression
-		PartExpression exp(name);
-		string parseErrorMsg;
-		std::auto_ptr<AstTop> ast = exp.parseExpressions( parseErrorMsg );
-		if (!ast.get()) {
-			ss << "AlterCmd: change trigger: Failed to parse expression '" << name << "'.  " << parseErrorMsg << "\n";
-			ss << dump_args(options,paths) << "\n";
-			throw std::runtime_error( ss.str() );
-		}
+	   std::string error_msg = "AlterCmd: change trigger:";
+	   std::auto_ptr<AstTop> ast = Expression::parse_no_throw(name,error_msg);
+	   if (!ast.get()) {
+	      ss << error_msg << "\n" << dump_args(options,paths) << "\n";
+	      throw std::runtime_error( ss.str() );
+	   }
 		break; }
 
 	case AlterCmd::COMPLETE: {
-		if (options.size() != 3) {
-			ss << "AlterCmd: change complete: expected four args: change complete 'expression'  <path_to_node> ";
-			ss << " but found " << (options.size() + paths.size()) << " arguments. The expression must be quoted\n";
-			ss << dump_args(options,paths) << "\n";
-			throw std::runtime_error( ss.str() );
-		}
-		name = options[2];
+	   if (options.size() != 3) {
+	      ss << "AlterCmd: change complete: expected four args: change complete 'expression'  <path_to_node> ";
+	      ss << " but found " << (options.size() + paths.size()) << " arguments. The expression must be quoted\n";
+	      ss << dump_args(options,paths) << "\n";
+	      throw std::runtime_error( ss.str() );
+	   }
+	   name = options[2];
 
-		// Parse the expression
-		PartExpression exp(name);
-		string parseErrorMsg;
-		std::auto_ptr<AstTop> ast = exp.parseExpressions( parseErrorMsg );
-		if (!ast.get()) {
-			ss << "AlterCmd: change complete: Failed to parse expression '" << name << "'.  " << parseErrorMsg << "\n";
-			ss << dump_args(options,paths) << "\n";
-			throw std::runtime_error( ss.str() );
-		}
+	   std::string error_msg = "AlterCmd: change complete:";
+	   std::auto_ptr<AstTop> ast = Expression::parse_no_throw(name,error_msg);
+	   if (!ast.get()) {
+	      ss << error_msg << "\n" << dump_args(options,paths) << "\n";
+	      throw std::runtime_error( ss.str() );
+	   }
 		break;}
 
 	case AlterCmd::REPEAT: {
