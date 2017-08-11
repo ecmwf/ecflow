74ef2301bee169b7dfb3f7b718524b34520e2677
Avi Bahra <Avi.Bahra@ecmwf.int>
2017-08-11 11:22:07 +0100

    ECFLOW-1071 update zombie output formatting

--- Base/src/cts/TaskCmds.cpp
+++ Base/src/cts/TaskCmds.cpp
@@ -86,9 +86,8 @@ bool TaskCmd::authenticate(AbstractServer* as, STC_Cmd_ptr& theReply) const
 		(void)as->zombie_ctrl().handle_path_zombie(as,this,action_taken,theReply);
 
 		// distinguish output by using *path*
-        std::stringstream ss;
-        ss << " chd:" << ecf::Child::to_string( child_type() );
-        ss << " : zombie(*path*) : " << process_or_remote_id_ << " : " << jobs_password_ << " : action(" << action_taken << ")";
+		std::stringstream ss;
+		ss << " zombie(*path*) : chd:" << ecf::Child::to_string(child_type()) << " : " << path_to_submittable_ << " : "<< process_or_remote_id_ << " : " << jobs_password_ << " : action(" << action_taken << ")";
 		log(Log::ERR,ss.str());
 		return false;
 	}
@@ -152,8 +151,7 @@ bool TaskCmd::authenticate(AbstractServer* as, STC_Cmd_ptr& theReply) const
 
       if (!password_missmatch && !pid_missmatch ) {
          std::stringstream ss;
-         ss << " chd:" << ecf::Child::to_string(child_type());
-         ss << " : zombie(pid & password match)? : " << path_to_submittable_ << " : already active : action(fob)";
+         ss << " zombie(pid & password match)? : chd:" << ecf::Child::to_string(child_type()) << " : "  << path_to_submittable_ << " : already active : action(fob)";
          log(Log::WAR, ss.str() );
          theReply = PreAllocatedReply::ok_cmd();
          return false;
@@ -171,8 +169,7 @@ bool TaskCmd::authenticate(AbstractServer* as, STC_Cmd_ptr& theReply) const
          // If ECF_NONSTRICT_ZOMBIES be more forgiving
          if (submittable_->user_variable_exists("ECF_NONSTRICT_ZOMBIES")) {
             std::stringstream ss;
-            ss << " chd:" << ecf::Child::to_string( Child::COMPLETE );
-            ss << " : zombie(ECF_NONSTRICT_ZOMBIES) : " << path_to_submittable_ ;
+            ss << " zombie(ECF_NONSTRICT_ZOMBIES) : chd:" << ecf::Child::to_string(child_type()) << " : " << path_to_submittable_ ;
             if (password_missmatch) ss << " : passwd != [ task:"<< submittable_->jobsPassword()<<" child:" << jobs_password_ << " ]";
             if (pid_missmatch)      ss << " : pid != [ task:"<< submittable_->process_or_remote_id()<<" child:" << process_or_remote_id_ << " ]";
             ss << " : already complete : action(fob)";
@@ -194,8 +191,7 @@ bool TaskCmd::authenticate(AbstractServer* as, STC_Cmd_ptr& theReply) const
       if (child_type() == Child::ABORT) {
          if (!password_missmatch && !pid_missmatch ) {
             std::stringstream ss;
-            ss << " chd:" << ecf::Child::to_string( Child::ABORT );
-            ss << " : zombie(pid & password match)? : " << path_to_submittable_ << " : already aborted : action(fob)";
+            ss << " zombie(pid & password match)? : chd:" << ecf::Child::to_string(child_type()) << " : " << path_to_submittable_ << " : already aborted : action(fob)";
             log(Log::WAR, ss.str() );
             theReply = PreAllocatedReply::ok_cmd();
             return false;
@@ -218,13 +214,13 @@ bool TaskCmd::authenticate(AbstractServer* as, STC_Cmd_ptr& theReply) const
   		   // LOG failure: Include type of zombie.
   		   // ** NOTE **: the zombie may have been removed by user actions. i.e if fob and child cmd is abort | complete, etc
   		   std::stringstream ss;
-  		   ss << " chd:" << ecf::Child::to_string(child_type());
-  		   ss << " : zombie";
+  		   ss << " zombie";
   		   const Zombie& theZombie = as->zombie_ctrl().find(path_to_submittable_, process_or_remote_id_, jobs_password_ );
   		   if (!theZombie.empty() ) ss << "(" << theZombie.type_str() << ")";
 
+         ss << " : chd:" << ecf::Child::to_string(child_type());
          ss << " : " << path_to_submittable_ << "(" << NState::toString(submittable_state) << ")";
-         ss << " : " << process_or_remote_id_ << " : " << jobs_password_;
+         ss << " : " << process_or_remote_id_  << " : " << jobs_password_;
          if (submittable_allready_active)   ss << " : already active";
          if (submittable_allready_complete) ss << " : already complete";
          if (submittable_allready_aborted)  ss << " : already aborted";
