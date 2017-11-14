251007e0ecb1f201653b0ba6ee49b61e5ef0918b
Avi Bahra <Avi.Bahra@ecmwf.int>
2017-11-14 17:01:42 +0000

    ECFLOW-1138 improce core api, allow Date to use string constructor

--- ANattr/src/DateAttr.hpp
+++ ANattr/src/DateAttr.hpp
@@ -29,6 +29,7 @@ namespace ecf { class Calendar;} // forward declare class that is in a name spac
 class DateAttr {
 public:
    DateAttr(int day, int month, int year); // will throw std::out_of_range for if invalid date
+   DateAttr(const std::string&);           // will throw std::runtime_error for if invalid date
    DateAttr()
    : day_(0), month_(0), year_(0), makeFree_(false),
      state_change_no_(0) {} // for serialisation
