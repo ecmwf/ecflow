461e87d7f16d9c3be3f623f50a7c28c13a16a965
Avi Bahra <Avi.Bahra@ecmwf.int>
2017-12-20 16:52:48 +0000

    ECFLOW-1138 fix scratch test

--- Pyext/test/py_u_test.py
+++ Pyext/test/py_u_test.py
@@ -21,70 +21,10 @@ from ecflow import Alias, AttrType, Autocancel, CheckPt, ChildCmdType, Client, C
 import unittest 
 import sys
 import os
-import ecflow_test_util as Test
 
-class TestDataAquistionSolution(unittest.TestCase):
-    def setUp(self):
-        #!/usr/bin/env python2.7
-        import os
-        
-        defs = Defs()
-        suite = defs.add_suite("data_aquisition")
-        suite.add_repeat( RepeatDay(1) )
-        suite.add_variable("ECF_HOME",    os.getenv("HOME") + "/course")
-        suite.add_variable("ECF_INCLUDE", os.getenv("HOME") + "/course")
-        suite.add_variable("ECF_FILES",   os.getenv("HOME") + "/course/data")
-        suite.add_variable("SLEEP","2")
-        for city in ( "Exeter", "Toulouse", "Offenbach", "Washington", "Tokyo", "Melbourne", "Montreal" ) :
-            fcity = suite.add_family(city)
-            fcity.add_task("archive")
-            for obs_type in ( "observations", "fields", "images" ):
-                type_fam = fcity.add_family(obs_type)
-                if city in ("Exeter", "Toulouse", "Offenbach"): type_fam.add_time("00:00 23:00 01:00")
-                if city in ("Washington") :                     type_fam.add_time("00:00 23:00 03:00")
-                if city in ("Tokyo") :                          type_fam.add_time("12:00")
-                if city in ("Melbourne") :                      type_fam.add_day( "monday" )
-                if city in ("Montreal") :                       type_fam.add_date(1, 0, 0)
-         
-                type_fam.add_task("get")
-                type_fam.add_task("process").add_trigger("get eq complete")
-                type_fam.add_task("store").add_trigger("get eq complete")
-                    
-        print(defs)
-        self.defs = defs
-        
-    def test_me0(self):
-        #!/usr/bin/env python2.7
-        import os
-        
-        home = os.path.join(os.getenv("HOME"), "course")
-        defs = Defs() + Suite("data_aquisition",
-                            RepeatDay(1),
-                            Edit(ECF_HOME=home),
-                            Edit(ECF_INCLUDE=home),
-                            Edit(ECF_FILES=home + "/data"),
-                            Edit(SLEEP=2))
-        for city in ( "Exeter", "Toulouse", "Offenbach", "Washington", "Tokyo", "Melbourne", "Montreal" ) :
-            fcity = defs.data_aquisition.add_family(city)
-            fcity += Task("archive")
-            for obs_type in ( "observations", "fields", "images" ):
-                type_fam = fcity.add_family(obs_type)
-                if city in ("Exeter", "Toulouse", "Offenbach"): type_fam + Time("00:00 23:00 01:00")
-                if city in ("Washington") :                     type_fam + Time("00:00 23:00 03:00")
-                if city in ("Tokyo") :                          type_fam + Time("12:00")
-                if city in ("Melbourne") :                      type_fam + Day( "monday" )
-                if city in ("Montreal") :                       type_fam + Date(1, 0, 0)
-         
-                type_fam >> Task("get") >> Task("process") >> Task("store") 
-                #type_fam.process += Trigger("get eq complete") 
-                #type_fam.store += Trigger("get eq complete")     
-
-        print(defs)
-        Ecf.set_debug_equality(True)
-        equals = (self.defs == defs)
-        Ecf.set_debug_equality(False)
-        self.assertEqual(self.defs, defs, "defs not equal")
-        
+class Test_me(unittest.TestCase):
+    pass
+    
 if __name__ == "__main__":
     unittest.main()
     print("All Tests pass")
