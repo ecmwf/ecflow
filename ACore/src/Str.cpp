8671a9a65764eb61d05fdad20484fe2044c29cbc
Avi Bahra <Avi.Bahra@ecmwf.int>
2018-01-09 11:47:39 +0000

    refactor warn about names with windows line ending

--- ACore/src/Str.cpp
+++ ACore/src/Str.cpp
@@ -270,9 +270,11 @@ bool Str::valid_name(const std::string& name, std::string &msg)
       result = name.find_first_not_of(VALID_NODE_CHARS, 1) == string::npos;
       if ( !result ) {
          msg = "Valid names can only consist of alphanumeric characters "
-               ",underscores and dots. The first character can not be a dot: '";
-         msg += name;
+               ",underscores and dots. The first character can not be a dot. ";
+         if (name.find('\r'))  msg += "Window's line ending ? ";
          msg += "'";
+         msg += name;
+         msg += "'"; // use '<name>' to show if PC format. i.e carriage return
       }
    }
 
