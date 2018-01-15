a7bec870417e7e8360f230aec52bb0677c523ae3
Avi Bahra <Avi.Bahra@ecmwf.int>
2018-01-15 13:58:32 +0000

    Update for macosx

--- bamboo/macosx1010-flags.cmake
+++ bamboo/macosx1010-flags.cmake
@@ -1,4 +1,4 @@
 # On mac appears no static python boost lib, hence use shared for everything
-SET( ENABLE_STATIC_BOOST_LIBS   OFF CACHE BOOL "Use dynamic boost libs")
+#SET( ENABLE_STATIC_BOOST_LIBS   OFF CACHE BOOL "Use dynamic boost libs")
 SET( ENABLE_PYTHON_PTR_REGISTER  ON CACHE BOOL "Register shared ptr to python converters")
 
