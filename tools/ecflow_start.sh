1a6ace278612ad8c78fc3e49645601983cc2e7d9
Avi Bahra <Avi.Bahra@ecmwf.int>
2018-06-11 10:17:50 +0100

    ECFLOW-1303 fix ecflow_start.h invocation
    Former-commit-id: 47b8178b9a655842dfeb724d2353f1ac604b5fdc

--- tools/ecflow_start.sh
+++ tools/ecflow_start.sh
@@ -126,7 +126,7 @@ localh=$(uname -n)
 # Update host, ecflow_site.sh is configured from CMAKE at install time
 # =========================================================================
 if [ -f ecflow_site.sh ] ; then
-$(source ./ecflow_site.sh)
+    source ./ecflow_site.sh
 fi
 
 # ==================================================================================
