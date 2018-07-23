95c5a7e34cfdd4f4231641324a4d67423612b7ac
Avi Bahra <Avi.Bahra@ecmwf.int>
2018-07-23 10:01:56 +0100

    fix for mac
    Former-commit-id: 62e024954f2d942f64583811651b83db91463197

--- tools/ecflow_standalone.c
+++ tools/ecflow_standalone.c
@@ -128,7 +128,7 @@ int main(argc,argv) int argc; char **argv;
      for (n=3; n<65535 ;n++) fclose(n); */
 
   /* create a new session from the child process */
-#if defined(linux) || defined (hpux) || defined (solaris) || defined (SGI) || defined (SVR4) || defined (AIX) || defined(SYG) || defined(alpha)
+#if defined(linux) || defined(__APPLE__) || defined(__MACH__) || defined(hpux) || defined(solaris) || defined(SGI) || defined(SVR4) || defined(AIX) || defined(SYG) || defined(alpha)
   if( setsid() == -1 )
 #else
   if( setsid(0) == -1 )
