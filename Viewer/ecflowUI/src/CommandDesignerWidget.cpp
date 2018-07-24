639110fa5e13d6d103f65ba4673d534c936d106d
Avi Bahra <Avi.Bahra@ecmwf.int>
2018-07-24 16:03:43 +0100

    ECFLOW-1065 add missing include
    Former-commit-id: d51ab5c87792935b79ffcb8e86e512a96b65a819

--- Viewer/ecflowUI/src/CommandDesignerWidget.cpp
+++ Viewer/ecflowUI/src/CommandDesignerWidget.cpp
@@ -8,6 +8,7 @@
 //============================================================================
 
 #include <boost/bind.hpp>
+#include <boost/enable_shared_from_this.hpp>
 
 #include <QMessageBox>
 
@@ -216,7 +217,7 @@ std::vector<VInfo_ptr>& CommandDesignerWidget::selectedNodes()
 void CommandDesignerWidget::addClientCommandsToComponentList()
 {
 	// sort the commands into alphabetical order
-	std::vector< std::shared_ptr<po::option_description> > options = clientOptionsDescriptions_->options();
+	std::vector< boost::shared_ptr<po::option_description> > options = clientOptionsDescriptions_->options();
 
 	std::sort(options.begin(),options.end(),
 		boost::bind(std::less<std::string>(),
