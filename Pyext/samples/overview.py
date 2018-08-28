e3dd5d13454d8ee7c37cfca71c64af5af9828759
Axel Bonet <map@ecmwf.int>
2018-08-28 12:29:43 +0000

    share overview.py, start with table display
    Former-commit-id: f1d7033fedab7be6628bfcbe695903449d9973a7

--- Pyext/samples/overview.py
+++ Pyext/samples/overview.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+# -*- coding= UTF-8 -*-
 """ tkinter example with ecflow
 """
-
+from __future__ import with_statement, print_function
 import Tkinter as tki
+from Tkinter import *
 import Queue
 import sys
 import time
@@ -33,6 +35,24 @@ COLORS = {"aborted": "red",
           "unknwon": "grey"}
 running = [True]
 
+ONCE = False
+
+
+def show_check(one):
+    print(one)
+    sep = ','
+    if sep not in one: return
+    exp = one.split(sep)
+    if len(exp) != 4: return
+    host, qid = exp[2], exp[3]
+    check = os.getenv("CHECK_TASK", "check_task.py")
+    global ONCE
+    if check is not None:
+        print(check +  " -n %s -j %s" % (host, qid))
+    elif ONCE:
+        print("#WAR: export CHECK_TASK=$HOME/bin/check_task.py; overview.py $ECF_HOST@$ECF_PORT,")
+        ONCE = True
+
 
 class Label(object):
     """ a class to encapsulate what was a global variable"""
@@ -52,11 +72,18 @@ class MenuBar(tki.Frame):
 
     def __init__(self, parent):
         tki.Frame.__init__(self, parent)
+        self.parent = parent
         self.__helpButton = self.__createHelp()
         self.__helpButton.grid(row=0, column=3)
+        self.__quitButton = tki.Button(self, text='Quit', font=BUTTON_FONT,
+                                       command=root.destroy)
+        self.__quitButton.grid(row=0, column=1)
         self.__updateButton = tki.Button(self, text='Update', font=BUTTON_FONT,
                                          command=parent.update)
         self.__updateButton.grid(row=0, column=2)
+
+    def quit(self, event=None):
+        self.parent.quit()
 
     def __createHelp(self):
         mb = tki.Menubutton(self, font=BUTTON_FONT,
@@ -73,31 +100,67 @@ class MenuBar(tki.Frame):
         return mb
 
     def __url(self, url=None):
-        if url is None:
-            return
-        os.system("firefox " + url)
+        if url is not None:
+            os.system("${BROWSER:=firefox} %s" % url)
 
 
 class TaskList(tki.Frame):
     """ a class to host all tasks"""
-    WIDTH = 40
-    LINES = 80
+    # WIDTH = w / 30
+    # LINES = h / 10
 
     def __init__(self, parent, kind):
-        tki.Frame.__init__(self, parent)
+        tki.Frame.__init__(self, parent, 
+                           # side="bottom", 
+                           # fill="both", 
+                           # expand=True,
+                           # sticky="nsew",
+        )
+        self.WIDTH = 80
+        self.LINES = 80
         self.__kind = kind
         self.__callback = None
-        self.__label = tki.Label(
-            self, font=BUTTON_FONT, background=COLORS[kind], text=kind)
-        self.__label.grid(row=0, column=0, sticky=tki.W)
+        #self.__label = tki.Label(
+        #    self, font=BUTTON_FONT, background=COLORS[kind], text=kind)
+        #self.__label.grid(row=0, column=0, sticky=tki.W)
+        select = tki.Button(self, text=kind,
+                                 font=BUTTON_FONT,
+                                 background=COLORS[kind],
+                                 command=self.select)
+        select.grid(row=1, column=0, sticky=tki.W)
+
+        selectall = tki.Button(self, text="select all",
+                                    font=BUTTON_FONT,
+                                    # background=COLORS[kind],
+                                    command=self.select_all)
+        selectall.grid(row=2, column=0, sticky=tki.W)
+
         self.__scrolledList = ScrolledList(
-            self, width=self.WIDTH, height=self.LINES,
+            self, 
+            # sticky="nsew",
+            width=self.WIDTH, 
+            height=self.LINES,  # selectmode=tki.EXTENDED,
             callback=self.__callback)
-        self.__scrolledList.grid(row=1, column=0)
+        self.__scrolledList.grid(row=3, column=0)
 
-    def insert(self, path):  self.__scrolledList.append(path)
+    def select_all(self):
+        res = set()
+        item = self.__scrolledList.listbox.get(0, tki.END)
+        for one in item:
+            print(one)
 
-    def clear(self):         self.__scrolledList.clear()
+    def select(self):
+        res = set()
+        selected = self.__scrolledList.listbox.curselection()
+        for i in selected:
+            item = self.__scrolledList.listbox.get(i)
+            res.add(item)
+            for one in res: 
+                show_check(one)
+                
+    def insert(self, path): self.__scrolledList.append(path)
+
+    def clear(self): self.__scrolledList.clear()
 
 
 class PaceKeeper():
@@ -124,31 +187,25 @@ class PaceKeeper():
 class Client(object):
     """ a class to focus on client-ecFlow-server comm"""
 
-    def __init__(self, one="local-localhost@31415"):
-        try:
-            nick, hhh = one.split("-")
-        except:
-            hhh = one
-            nick = None
-        try:
-            host, port = hhh.split("@")
-        except:
-            host = "localhost"
-            port = 31415
-
-        if nick is None:
-            self.nick = "%s@%d" % (host, port)
-        print "# client creation", nick, host, port
-        self.nick = nick
+    def __init__(self, one="localhost@31415"):
+        # try: nick, hhh = one.split("-")
+        # except: hhh = one; nick = None
+        if "@" in one:
+            host, port = one.split("@")
+        # except: 
+        else: host = "localhost"; port = 31415
+        self.nick = one
+        # if nick is None: self.nick = "%s@%s" % (host, port)
+        print("# client creation", self.nick, host, port)
         self.client = ec.Client(host, port)
 
     def process(self, win):
-        Label.update()
+        # Label.update()
         self.client.sync_local()
         defs = self.client.get_defs()
         if defs is None:
             print("# %s-%: empty content" % (self.host, self.port))
-        Label.update()
+        # Label.update()
         for suite in defs.suites:
             self.process_nc(suite, win)
 
@@ -159,12 +216,21 @@ class Client(object):
             else:
                 self.process_nc(item, win)
 
-    def process_node(self, node, wins):
+    def process_node(self, item, wins):
         for kind, win in wins.items():
-            status = "%s" % node.get_state()
+            status = "%s" % item.get_state()
             if status != kind:
                 continue
-            win.insert("%s:%s" % (self.nick, node.get_abs_node_path()))
+            change = item.get_state_change_time()
+            line = "#%s:%s,%s" % (
+                self.nick, item.get_abs_node_path(), change)
+            try:
+                import check_task as ct
+                qid = str(ct.display_var(item, "qid"))
+                host = ct.display_var(item, "ECF_JOB_CMD")
+                line += ",%s,%s" % (host, qid)
+            except: pass
+            win.insert(line)
 
 try:
     sys.path.append('/usr/local/apps/sms/lib/')
@@ -194,7 +260,7 @@ class ClientSMS(Client):
         user = get_username()
 
         if DEBUG:
-            print "#MSG: login:", nick, host, port, user
+            print("#MSG: login:", nick, host, port, user)
         handle, rc = cdp.sms_client_login(host, user, passwd,
                                           timeout, port, tree)
         self.handle = handle
@@ -219,7 +285,7 @@ class ClientSMS(Client):
                 path = cdp.sms_node_full_name(node)
                 win.insert("%s:%s" % (self.nick, path))
                 if DEBUG:
-                    print self.host, self.port, path, state
+                    print(self.host, self.port, path, state)
 
     def process(self, win):
         rc = cdp.sms_client_news(self.handle, self.num)
@@ -253,40 +319,59 @@ class ClientSMS(Client):
 
     def __exit__(self):
         cdp.sms_client_logout(self.handle)
-        print "# logout from", self.nick
+        print("# logout from", self.nick)
 
     def __delete__(self):
         cdp.sms_client_logout(self.handle)
-        print "# logout from", self.nick
+        print("# logout from", self.nick)
+
 
 
 class Application(tki.Frame):
     """ main """
 
-    def __init__(self, master=None, client=None, queue=None):
-        tki.Frame.__init__(self, master)
+    def __init__(self, parent=None, client=None, queue=None):
+        if parent is None: parent = tki.Tk()
+        w, h = parent.winfo_screenwidth(), parent.winfo_screenheight()
+        tki.Frame.__init__(self, parent)
         if client is None:
-            self.__clients = [Client("localhost@31415"), ]
-        elif type(client) == set:
+            raise Exception("#ERR: please provide a client...\n./overview.py $ECF_HOST@$ECF_PORT")
+        elif type(client) in (set, tuple):
             self.__clients = client
         else:
             self.__clients = [client]
-
         self.__queue = queue
-        width = 640
-        height = 780
+        # width = 640;         height = 780
+        width = w;         height = h
         self.canvas = tki.Canvas(width=width, height=height, bg='black')
         self.grid()
         self.createWidgets()
         self.canvas.after(50, self.check_queue)
 
+        self.bind_all("<space>", self.update)
+        # self.bind_all("Control_q", self.quit)
+        # self.bind_all("Control_z", self.resize)
+        self.bind_all("<Control-q>", self.quit)
+        self.bind_all("<Control-z>", self.resize)
+
+    def resize(self, event=None): print("New size is: {}x{}".format(event.width, event.height))
+
+    def quit(self, event=None):
+        print("quit...")
+        self.destroy()
+        # global running; running = [ False, ]
+        # sys.exit(0)
+
     def createWidgets(self):
-        rowx = 1
-        glob = Label(tki.StringVar(self))
+        global root
         root = self
+        rowx = 1
+        var = tki.StringVar(self)
+        glob = Label(root)
+        # glob = Label(var)
         self.__menuBar = MenuBar(root)
         self.__menuBar.grid(row=0, column=0, sticky=tki.W)
-        self.label = tki.Label(root, textvariable=Label.inst)
+        self.label = tki.Label(root, textvariable=var) # Label.inst)
         self.label.grid(row=0, column=2, sticky=tki.E)
         self.__wins = dict()
         rowx += 1
@@ -308,8 +393,8 @@ class Application(tki.Frame):
             self.update()
         self.canvas.after(50, self.check_queue)
 
-    def update(self):
-        Label.update()
+    def update(self, event=None):
+        self.label.update()
         for kind, win in self.__wins.items():
             win.clear()
         for client in self.__clients:
@@ -317,10 +402,7 @@ class Application(tki.Frame):
                 for clt in client:
                     clt.process(self.__wins)
             else:
-                try:
-                    client.process(self.__wins)
-                except:
-                    pass
+                client.process(self.__wins)
 
 
 def get_username(): return pwd.getpwuid(os.getuid())[0]
@@ -335,23 +417,30 @@ if __name__ == '__main__':
     except:
         port = 31415
 
-    if len(sys.argv) > 0:
+    if len(sys.argv) > 1:
         clients = []
-        for num in xrange(1, len(sys.argv)):
-            clients.append(Client(sys.argv[num]))
-    else:
-        clients = [Client("localhost%d" % port)]
+        for num in xrange(1, len(sys.argv)):            
+            sep = ','
+            arg = sys.argv[num]
+            if sep in arg:
+                for one in arg.split(sep):
+                    client = Client(one)
+            else:
+                client = Client(arg)
+            clients.append(client)
+    else: 
+        one = "localhost%d" % port
+        print("# using", one)
+        clients = [Client(one), ]
 
     queue = Queue.Queue()
-    # app = AppSms(host, port, queue)
     app = Application(client=clients, queue=queue)
     app.master.title(PROGRAM_NAME)
     app.columnconfigure(0, weight=1)
     app.rowconfigure(0, weight=1)
-
     PaceKeeper(app, queue)
     app.mainloop()
 
 """
-python ./overview.py localhost-localhost@31415
+python ./overview.py localhost@31415,
 """
