356fec69b43b7ecba6406be9912a74dc4fd95878
Axel Bonet <map@ecmwf.int>
2018-08-28 12:29:43 +0000

    share overview.py, start with table display
    Former-commit-id: 1a697dcb61406dbb411b1decb8427b9f03415921

--- Viewer/ecflowUI/src/TableNodeModel.hpp
+++ Viewer/ecflowUI/src/TableNodeModel.hpp
@@ -27,68 +27,70 @@ class TableNodeModel : public AbstractNodeModel
 {
 Q_OBJECT
 
-    friend class TableNodeSortModel;
-
+  friend class TableNodeSortModel;
+  
 public:
-   	TableNodeModel(ServerFilter* serverFilter,NodeFilterDef* filterDef,QObject *parent=0);
+  TableNodeModel(ServerFilter* serverFilter,NodeFilterDef* filterDef,QObject *parent=0);
 
-	int columnCount (const QModelIndex& parent = QModelIndex() ) const;
-   	int rowCount (const QModelIndex& parent = QModelIndex() ) const;
-
-   	QVariant data (const QModelIndex& , int role = Qt::DisplayRole ) const;
-	QVariant headerData(int,Qt::Orientation,int role = Qt::DisplayRole ) const;
-
-   	QModelIndex index (int, int, const QModelIndex& parent = QModelIndex() ) const;
-   	QModelIndex parent (const QModelIndex & ) const;
-
-   	VInfo_ptr nodeInfo(const QModelIndex&);
-    void selectionChanged(QModelIndexList lst);
-
-   	VModelData* data() const;
-    ModelColumn* columns() const {return columns_;}
-
-    //To speed up identifying a column. The mapping here must match the definition of
-    //"table_columns" in ecflowview_view_conf.json !!!
-    enum ColumnType {PathColumn=0,StatusColumn=1,TypeColumn=2,TriggerColumn=3,
-                     LabelColumn=4, EventColumn=5, MeterColumn=6, StatusChangeColumn=7};
-
+  int columnCount (const QModelIndex& parent = QModelIndex() ) const;
+  int rowCount (const QModelIndex& parent = QModelIndex() ) const;
+  
+  QVariant data (const QModelIndex& , int role = Qt::DisplayRole ) const;
+  QVariant headerData(int,Qt::Orientation,int role = Qt::DisplayRole ) const;
+  
+  QModelIndex index (int, int, const QModelIndex& parent = QModelIndex() ) const;
+  QModelIndex parent (const QModelIndex & ) const;
+  
+  VInfo_ptr nodeInfo(const QModelIndex&);
+  void selectionChanged(QModelIndexList lst);
+  
+  VModelData* data() const;
+  ModelColumn* columns() const {return columns_;}
+  
+  //To speed up identifying a column. The mapping here must match the definition of
+  //"table_columns" in ecflowview_view_conf.json !!!
+  enum ColumnType {PathColumn=0,StatusColumn=1,TypeColumn=2,TriggerColumn=3,
+		   LabelColumn=4, EventColumn=5, MeterColumn=6, StatusChangeColumn=7,
+		   PreviousStart=8, PreviousStop=9, Rid=10,
+  };
+   
 public Q_SLOTS:
-    void slotServerAddBegin(int);
-   	void slotServerAddEnd();
-    void slotServerRemoveBegin(VModelServer* server,int);
-    void slotServerRemoveEnd(int);
-
-   	void slotDataChanged(VModelServer*) {}
-    void slotNodeChanged(VTableServer*,const VNode*);
-    void slotAttributesChanged(VModelServer*,const VNode*) {}
-    void slotBeginAddRemoveAttributes(VModelServer*,const VNode*,int,int) {}
-    void slotEndAddRemoveAttributes(VModelServer*,const VNode*,int,int) {}
-
-   	void slotBeginServerScan(VModelServer* server,int);
-   	void slotEndServerScan(VModelServer* server,int);
-   	void slotBeginServerClear(VModelServer* server,int);
-   	void slotEndServerClear(VModelServer* server,int);
-
+  void slotServerAddBegin(int);
+  void slotServerAddEnd();
+  void slotServerRemoveBegin(VModelServer* server,int);
+  void slotServerRemoveEnd(int);
+  
+  void slotDataChanged(VModelServer*) {}
+  void slotNodeChanged(VTableServer*,const VNode*);
+  void slotAttributesChanged(VModelServer*,const VNode*) {}
+  void slotBeginAddRemoveAttributes(VModelServer*,const VNode*,int,int) {}
+  void slotEndAddRemoveAttributes(VModelServer*,const VNode*,int,int) {}
+  
+  void slotBeginServerScan(VModelServer* server,int);
+  void slotEndServerScan(VModelServer* server,int);
+  void slotBeginServerClear(VModelServer* server,int);
+  void slotEndServerClear(VModelServer* server,int);
+  
 Q_SIGNALS:
-    void filterChangeBegun();
-    void filterChangeEnded();
+  void filterChangeBegun();
+  void filterChangeEnded();
 
 protected:
-   	bool isServer(const QModelIndex & index) const {return false;}
-	ServerHandler* indexToRealServer(const QModelIndex & index) const {return NULL;}
-	VModelServer* indexToServer(const QModelIndex & index) const {return NULL;}
-	QModelIndex serverToIndex(ServerHandler*) const {return QModelIndex();}
-
-    QModelIndex nodeToIndex(VTableServer* server,const VNode* node, int column) const;
-    QModelIndex nodeToIndex(const VNode*,int column=0) const;
-    VNode* indexToNode( const QModelIndex & index) const;
-
-    QModelIndex attributeToIndex(const VAttribute* a, int column=0) const;
-
-	QVariant nodeData(const QModelIndex& index,int role) const;
-
-	VTableModelData* data_;
-	ModelColumn* columns_;
+  bool isServer(const QModelIndex & index) const {return false;}
+  ServerHandler* indexToRealServer(const QModelIndex & index) const {return NULL;}
+  VModelServer* indexToServer(const QModelIndex & index) const {return NULL;}
+  QModelIndex serverToIndex(ServerHandler*) const {return QModelIndex();}
+  
+  QModelIndex nodeToIndex(VTableServer* server,const VNode* node, int column) const;
+  QModelIndex nodeToIndex(const VNode*,int column=0) const;
+  VNode* indexToNode( const QModelIndex & index) const;
+  
+  QModelIndex attributeToIndex(const VAttribute* a, int column=0) const;
+  
+  QVariant nodeData(const QModelIndex& index,int role) const;
+  
+  VTableModelData* data_;
+  ModelColumn* columns_;
 };
 
 #endif
