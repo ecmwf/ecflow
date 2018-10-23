6a51be93827982a55d5a8193d9389766744e8814
Avi Bahra <Avi.Bahra@ecmwf.int>
2018-10-23 11:20:27 +0100

    Updare version to 4.11.1
    Former-commit-id: 84cb769e249ab4b15e9fe89494e245f7f90ae069

--- VERSION.cmake
+++ VERSION.cmake
@@ -1,7 +1,7 @@
 set( ECFLOW_RELEASE  "4" )
 set( ECFLOW_MAJOR    "11" )
-set( ECFLOW_MINOR    "0" )
+set( ECFLOW_MINOR    "1" )
 
 # use this form to be consistent with other packages,+ allow standard extraction of version from scripts
 # could replaced "4.0.8" with "${ECFLOW_RELEASE}.${ECFLOW_MAJOR}.${ECFLOW_MINOR}"
-set( ${PROJECT_NAME}_VERSION_STR  "4.11.0" )
+set( ${PROJECT_NAME}_VERSION_STR  "4.11.1" )
