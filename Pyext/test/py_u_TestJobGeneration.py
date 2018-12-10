7fcd2ba76a99dc763860afa0ebc8fa2f081dd3a4
Avi Bahra <Avi.Bahra@ecmwf.int>
2018-12-10 12:51:18 +0000

    ECFLOW-1412 allow python2 and python3 test to run in parallel
    Former-commit-id: 7607a2141f564f15b4517baabc625bca5bda9080

--- Pyext/test/py_u_TestJobGeneration.py
+++ Pyext/test/py_u_TestJobGeneration.py
@@ -19,11 +19,13 @@ import shutil   # used to remove directory tree
 import ecflow_test_util as Test
 from ecflow import Defs, JobCreationCtrl, TaskVec, File, Client, debug_build
 
-def ecf_includes() : return File.source_dir() + "/Pyext" + "/test/data/includes"
+def ecf_includes() : return File.source_dir() + "/Pyext/test/data/includes"
+
+def suite_name(): return "suite_job_gen_" + str(os.getpid())  # allow python2 and python3 to run in parallel
 
 def create_defs(ecf_home,task_vec):
     defs = Defs();
-    suite = defs.add_suite("suite_job_gen");
+    suite = defs.add_suite( suite_name() );
     suite.add_variable("ECF_HOME",ecf_home);
     suite.add_variable("ECF_CLIENT_EXE_PATH",File.find_client());
     suite.add_variable("SLEEPTIME","1");
@@ -42,6 +44,24 @@ def create_defs(ecf_home,task_vec):
     task_vec.append(t2);
     task_vec.append(t3);
     task_vec.append(t4);
+    
+    # Create dir. **NOTE**, if the second component of os.path.joind(..) is absolute, 
+    # the first path is ignored, hence remove first char
+    dir_to_create = os.path.join(str(ecf_home),fam.get_abs_node_path()[1:])
+    print("Creating dir ",dir_to_create)
+    os.makedirs( dir_to_create  )
+     
+    # copy ecf files from ecflow source, to create unique file per process
+    src_t1  = os.path.join(ecf_home, "suite_job_gen" , "family" , "t1.ecf")
+    dest_t1 = os.path.join(ecf_home,  t1.get_abs_node_path()[1:] + ".ecf" )
+    src_t2  = os.path.join(ecf_home, "suite_job_gen" , "family" , "t2.ecf")
+    dest_t2 = os.path.join(ecf_home,  t2.get_abs_node_path()[1:] + ".ecf" )
+    src_t3  = os.path.join(ecf_home, "suite_job_gen" , "family" , "t3.ecf")
+    dest_t3 = os.path.join(ecf_home,  t3.get_abs_node_path()[1:] + ".ecf" )
+ 
+    shutil.copy(src_t1, dest_t1)
+    shutil.copy(src_t2, dest_t2)
+    shutil.copy(src_t3, dest_t3)
     
     return defs
 
@@ -73,16 +93,14 @@ def check_jobs(task_vec, ecf_home):
             assert False, "Could not find job file " + the_job_file
 
 
-
-
 if __name__ == "__main__":
     print("####################################################################")
     print("Running ecflow version " + Client().version() + " debug build(" + str(debug_build()) +")")
     print("####################################################################")
- 
+    
     workspace = File.source_dir();
     print(workspace)
-    
+
     ecf_home = workspace + "/Pyext/test/data/ECF_HOME"
     task_vec = TaskVec()
     defs = create_defs(ecf_home,task_vec)
@@ -114,10 +132,9 @@ if __name__ == "__main__":
     defs.check_job_creation(job_ctrl)
     print(job_ctrl.get_error_msg())
     
-    generated_dir = job_ctrl.get_dir_for_job_creation() + "/suite_job_gen"
+    generated_dir = job_ctrl.get_dir_for_job_creation() + "/" + suite_name()
     print("removing directory tree " + generated_dir)
     shutil.rmtree(generated_dir)      
- 
  
     # Create jobs for all task, to a TMP directory ($TMPDIR/ecf_check_job_creation/ECF_NAME.job0
     # When run via rsh then TMPDIR may not be defined, hence expect exception to be thrown
@@ -128,8 +145,14 @@ if __name__ == "__main__":
         print(job_ctrl.get_error_msg())
         print("removing directory tree " + job_ctrl.get_dir_for_job_creation())
         shutil.rmtree(job_ctrl.get_dir_for_job_creation())     
-    
         delete_jobs(task_vec,ecf_home)
+    except RuntimeError as e:
+        print("failed: " + str(e))  
+        
+    try:
+        dir_to_del = os.path.join(ecf_home,suite_name())
+        print("removing directory tree " + dir_to_del )
+        shutil.rmtree(dir_to_del)     
         print("All test pass")
     except RuntimeError as e:
         print("failed: " + str(e))    
