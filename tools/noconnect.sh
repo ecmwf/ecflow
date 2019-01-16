14aad697e7d41037d988743c8c9933f627d3d47b
Avi Bahra <Avi.Bahra@ecmwf.int>
2019-01-16 12:53:53 +0000

    ECFLOW-1437 update copyright
    Former-commit-id: 6066b3f34db3e3bc8b4245a12dfb06f303e6f79c

--- tools/noconnect.sh
+++ tools/noconnect.sh
@@ -7,7 +7,7 @@
 ##.AUTHOR   
 ## Revision    : $Revision: #7 $ 
 ##
-## Copyright 2009-2017 ECMWF.
+## Copyright 2009-2019 ECMWF.
 ## This software is licensed under the terms of the Apache Licence version 2.0 
 ## which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
 ## In applying this licence, ECMWF does not waive the privileges and immunities 
