88a3c603dc5fc78e6992e776f5a388987cb13505
Avi Bahra <Avi.Bahra@ecmwf.int>
2019-03-25 15:54:31 +0000

    avoid hard coding port
    Former-commit-id: cc32a31ec9e77611e772a20b7d373bffd2fc169b

--- build_scripts/nightly/load.py
+++ build_scripts/nightly/load.py
@@ -5,11 +5,11 @@ import ecflow
 # =====================================================================================
 print "delete the definition in the server"
 # ======================================================================================
-ci = ecflow.Client("localhost:4141")
+ci = ecflow.Client()
 try:
     ci.restart_server();
     ci.delete_all(True)           # clear out the server
-    #ci.load(generated_defs_file)  # load the definition into the server, from disk
+    #ci.load(generated_defs_file) # load the definition into the server, from disk
     #ci.load(defs)                # load the definition into the server, from memory
     #ci.replace("/suite",generated_defs_file,True,True)     
     ci.begin_all_suites()         # need only begin once 
