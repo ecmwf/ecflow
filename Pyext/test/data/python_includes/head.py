9e9d2914175cfee1a86667096532851506ce3633
Avi Bahra <Avi.Bahra@ecmwf.int>
2019-06-18 13:45:43 +0100

    Update python head
    Former-commit-id: c53cf30f546be80cc46eb0ee2e8caba7317673da

--- Pyext/test/data/python_includes/head.py
+++ Pyext/test/data/python_includes/head.py
@@ -12,15 +12,25 @@ ci.set_child_path("%ECF_NAME%")
 ci.set_child_password("%ECF_PASS%")
 ci.set_child_try_no(%ECF_TRYNO%)
 
-print("Only wait 5  minutes, if the server cannot be contacted (note default is 24 hours) before failing")
-ci.set_child_timeout(300) 
+print("Only wait 2 minutes, if the server cannot be contacted (note default is 24 hours) before failing")
+ci.set_child_timeout(120) 
 
+try:
+   ci.ping();
+   print("ping ok")
+except RuntimeError as e:
+   print("Error: abort in head.h for ci.ping()" + str(e))
+   ci.child_abort("Abort in head for ci.ping()" + str(e))
+except:
+   print("Error: abort in head.h for ci.ping()")
+   ci.child_abort("Abort in head for ci.ping()")
+   
 try:
    ci.child_init();
    print("child init ok")
 except RuntimeError as e:
-   print("Error: abort in head.h " + str(e))
-   ci.child_abort("Abort in head" + str(e))
+   print("Error: abort in head.h for ci.child_init()" + str(e))
+   ci.child_abort("Abort in head for ci.child_init()" + str(e))
 except:
-   print("Error: abort in head.h")
-   ci.child_abort("Abort in head")
+   print("Error: abort in head.h for ci.child_init()")
+   ci.child_abort("Abort in head for ci.child_init()")
