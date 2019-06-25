0f72cf62da1e918c17619edac19e1fc429a33630
Avi Bahra <Avi.Bahra@ecmwf.int>
2019-06-25 11:26:41 +0100

    ECFLOW-882 add basic test
    Former-commit-id: d7719e0bb5f2d75811eb3f60d42e12723305dd9a

--- build_scripts/nightly/quick_install.sh
+++ build_scripts/nightly/quick_install.sh
@@ -60,6 +60,7 @@ git checkout master
 # test suites
 # ========================================================================
 cd $WK
+python Pyext/samples/TestBench.py  ANode/parser/test/data/good_defs/limit/basic.def
 python Pyext/samples/TestBench.py  ANode/parser/test/data/good_defs/trigger/all_trigger_examples.def
 python Pyext/samples/TestBench.py  ANode/parser/test/data/single_defs/test_time_why.def
 
