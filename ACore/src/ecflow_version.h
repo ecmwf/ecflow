343583f3e4b3ff669703b4a5fcb2e477d66a315c
Avi Bahra <Avi.Bahra@ecmwf.int>
2019-08-15 14:13:06 +0100

    Update to new release
    Former-commit-id: 6ab2d602cc9ade8e2e6e197310c64a7e1369a365

--- ACore/src/ecflow_version.h
+++ ACore/src/ecflow_version.h
@@ -1,9 +1,9 @@
 #ifndef ecflow_version_config_h
 #define ecflow_version_config_h
  
-#define ECFLOW_VERSION   "4.16.0"
+#define ECFLOW_VERSION   "4.17.0"
 #define ECFLOW_RELEASE   "4"
-#define ECFLOW_MAJOR     "16"
+#define ECFLOW_MAJOR     "17"
 #define ECFLOW_MINOR     "0"
 
 #endif 
