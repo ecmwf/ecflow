bc9544ac4392597475fea868a2d6b32662c9700b
Avi Bahra <Avi.Bahra@ecmwf.int>
2019-11-15 11:48:27 +0000

    remove cppcheck supression
    Former-commit-id: f787845676bbd3ef774012d5a0e6a4562b3e3832

--- ACore/src/TimeStamp.cpp
+++ ACore/src/TimeStamp.cpp
@@ -31,7 +31,6 @@ void TimeStamp::now(std::string& time_stamp)
 {
    char t_fmt[255];
    time_t stamp = std::time( nullptr);
-   // cppcheck-suppress localtimeCalled
    struct tm *tod = localtime(&stamp);
    sprintf(t_fmt, "[%02d:%02d:%02d %d.%d.%d] ", tod->tm_hour, tod->tm_min, tod->tm_sec,
            tod->tm_mday, tod->tm_mon + 1, tod->tm_year + 1900);
@@ -44,7 +43,6 @@ void TimeStamp::now_in_brief(std::string& time_stamp)
 
    char t_fmt[255];
    time_t stamp = time( nullptr);
-   // cppcheck-suppress localtimeCalled
    struct tm *tod = localtime(&stamp);
    sprintf(t_fmt, "[%02d:%02d:%02d %d.%d] ", tod->tm_hour, tod->tm_min, tod->tm_sec,
            tod->tm_mday, tod->tm_mon + 1);
