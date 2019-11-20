39e553e72c1f583b440b459a2eb697427b6f1d88
Tiago Quintino <Tiago.Quintino@ecmwf.int>
2019-11-20 16:57:06 +0000

    Fix compilation macosx
    Former-commit-id: adf145f619365048ac1a6a6a92f1932c9367ab73

--- ANode/src/InLimitMgr.hpp
+++ ANode/src/InLimitMgr.hpp
@@ -3,18 +3,20 @@
 /////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
 // Name        :
 // Author      : Avi
-// Revision    : $Revision: #16 $ 
+// Revision    : $Revision: #16 $
 //
 // Copyright 2009-2019 ECMWF.
-// This software is licensed under the terms of the Apache Licence version 2.0 
-// which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
-// In applying this licence, ECMWF does not waive the privileges and immunities 
-// granted to it by virtue of its status as an intergovernmental organisation 
-// nor does it submit to any jurisdiction. 
+// This software is licensed under the terms of the Apache Licence version 2.0
+// which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+// In applying this licence, ECMWF does not waive the privileges and immunities
+// granted to it by virtue of its status as an intergovernmental organisation
+// nor does it submit to any jurisdiction.
 //
 // Description :
 /////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
 #include <set>
+#include <string>
+#include <memory>
 
 #include "InLimit.hpp"
 #include "LimitFwd.hpp"
