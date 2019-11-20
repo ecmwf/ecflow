0e59e773aa83e2cc1c597922f4ee7712c081a02d
Iain Russell <Iain.Russell@ecmwf.int>
2019-11-20 12:25:21 +0000

    fix compilation with boost 1.68.0
    Former-commit-id: cd992aaeb9b816a11eea7775c9fcbe2a799c7f68

--- ANattr/test/TestDateAttr.cpp
+++ ANattr/test/TestDateAttr.cpp
@@ -14,6 +14,8 @@
 // Description :
 /////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
 
+#include<iostream>
+
 #include <boost/test/unit_test.hpp>
 
 #include "DateAttr.hpp"
