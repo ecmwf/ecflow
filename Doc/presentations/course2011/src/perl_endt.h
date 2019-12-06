0ecbec8706b0e8838236c33abaa7c8c0dd983dc5
Avi Bahra <Avi.Bahra@ecmwf.int>
2019-12-06 15:18:32 +0000

    ECFLOW-1560 remove old presentations
    Former-commit-id: 17d1b2ed16e7276f7df7e36960c568f7f53d69e9

--- Doc/presentations/course2011/src/perl_endt.h
+++
@@ -1,9 +0,0 @@
-';
-if ($@){
-    print "caught signal: $@\n";
-    xabo(); # xabort();
-    exit;
-  }
-print "the job is now complete\n";
-xcom(); # xcomplete();
-exit;
--- Doc/presentations/course2011/src/python_endt.h
+++
@@ -1,3 +0,0 @@
-call_complete()
-# os.system('smscomplete')
-# os.system('ecflow_client --complete')
