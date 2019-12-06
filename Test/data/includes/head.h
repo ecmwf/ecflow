35ab96f9a3ac724d373e8cd16fd955dc12df59ef
Iain Russell <Iain.Russell@ecmwf.int>
2019-12-06 15:36:09 +0000

    Add copyright notice to Python samples
    Former-commit-id: a001af326d4b2443391fb9e3b31b8665d0af8800

--- Test/data/includes/head.h
+++ Test/data/includes/head.h
@@ -1,4 +1,16 @@
 #!/bin/ksh
+#/////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
+# Name        :
+# Author      : Avi
+# Revision    : $Revision: #5 $ 
+#
+# Copyright 2009-2019 ECMWF.
+# This software is licensed under the terms of the Apache Licence version 2.0 
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
+# In applying this licence, ECMWF does not waive the privileges and immunities 
+# granted to it by virtue of its status as an intergovernmental organisation 
+# nor does it submit to any jurisdiction. 
+#/////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
 
 %manual
 #This is the manual from the head.h file
