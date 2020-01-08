78948995d42201ddc5eff342f59c086ce719ce28
Avi Bahra <Avi.Bahra@ecmwf.int>
2020-01-08 12:39:15 +0000

    Update version after release
    Former-commit-id: fad1d435c8aee89ec28f0aef4c35ff8721b9242c

--- Pyext/ecflow/__init__.py
+++ Pyext/ecflow/__init__.py
@@ -15,6 +15,6 @@ from .ecflow import *
 The ecFlow python module
 """
 
-__version__ = '4.17.1'
+__version__ = '4.18.0'
 
 # http://stackoverflow.com/questions/13040646/how-do-i-create-documentation-with-pydoc
