1ee4ca54b51f225b0401289b6e49198120ed3c8b
Avi Bahra <Avi.Bahra@ecmwf.int>
2020-01-29 10:39:00 +0000

    Update for next release

--- ACore/src/ecflow_version.h
+++ ACore/src/ecflow_version.h
@@ -1,9 +1,9 @@
 #ifndef ecflow_version_config_h
 #define ecflow_version_config_h
  
-#define ECFLOW_VERSION   "5.2.1"
+#define ECFLOW_VERSION   "5.3.0"
 #define ECFLOW_RELEASE   "5"
-#define ECFLOW_MAJOR     "2"
-#define ECFLOW_MINOR     "1"
+#define ECFLOW_MAJOR     "3"
+#define ECFLOW_MINOR     "0"
 
 #endif 
