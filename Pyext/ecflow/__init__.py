fb367a92a850e9f797dd98abf0d4d45c440df205
Avi Bahra <Avi.Bahra@ecmwf.int>
2020-02-26 14:32:19 +0000

    Update version number

--- Pyext/ecflow/__init__.py
+++ Pyext/ecflow/__init__.py
@@ -15,6 +15,6 @@ from .ecflow import *
 The ecFlow python module
 """
 
-__version__ = '5.2.3'
+__version__ = '5.3.0'
 
 # http://stackoverflow.com/questions/13040646/how-do-i-create-documentation-with-pydoc
