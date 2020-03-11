dab081778d1259bdfd283cef44784cbadda5d76a
Avi Bahra <Avi.Bahra@ecmwf.int>
2020-03-11 15:54:57 +0000

    refactor

--- mac.sh
+++ mac.sh
@@ -17,5 +17,5 @@
 #
 
 cmake -DCMAKE_MODULE_PATH=${HOME}/bitbucket/ecbuild/cmake/ ${HOME}/bitbucket/ecflow/  \
-      -DCMAKE_CXX_FLAGS='-ftemplate-depth=512 -Wno-deprecated-declarations' -DBOOST_ROOT=/usr/local -DCMAKE_PREFIX_PATH=/usr/local/opt/qt \
+      -DCMAKE_CXX_FLAGS='-std=c++14 -ftemplate-depth=512 -Wno-deprecated-declarations' -DBOOST_ROOT=/usr/local -DCMAKE_PREFIX_PATH=/usr/local/opt/qt \
       -DCMAKE_INSTALL_PREFIX=${HOME}/install_test -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl
