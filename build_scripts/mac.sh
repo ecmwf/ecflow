fa4dc4059679d4b7918ff2253ddd968d5a422ace
Avi Bahra <Avi.Bahra@ecmwf.int>
2020-03-12 09:59:33 +0000

    refactor

--- build_scripts/mac.sh
+++ build_scripts/mac.sh
@@ -19,6 +19,9 @@
 #clang -x c -v -E /dev/null
 #
 
-cmake -DCMAKE_MODULE_PATH=${HOME}/bitbucket/ecbuild/cmake/ ${HOME}/bitbucket/ecflow/  \
-      -DCMAKE_CXX_FLAGS='-fvisibility=hidden -fvisibility-inlines-hidden -ftemplate-depth=512 -Wno-deprecated-declarations' -DBOOST_ROOT=/usr/local -DCMAKE_PREFIX_PATH=/usr/local/opt/qt \
-      -DCMAKE_INSTALL_PREFIX=${HOME}/install_test -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl
+cmake -DCMAKE_MODULE_PATH=${HOME}/git/ecbuild/cmake/ ${HOME}/git/ecflow/  \
+      -DCMAKE_CXX_FLAGS='-fvisibility=hidden -fvisibility-inlines-hidden -ftemplate-depth=512 -Wno-deprecated-declarations' \
+      -DBOOST_ROOT=/usr/local \
+      -DCMAKE_PREFIX_PATH=/usr/local/opt/qt \
+      -DCMAKE_INSTALL_PREFIX=${HOME}/install_test \
+      -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl
