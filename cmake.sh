2d3423d9fc9b487587b55a464ab62b31ea4bf3cb
Avi Bahra <Avi.Bahra@ecmwf.int>
2020-03-23 17:00:11 +0000

    experiment with version in Cmakelist.txt

--- cmake.sh
+++ cmake.sh
@@ -279,9 +279,10 @@ fi
 # ====================================================================================
 # Use for local install
 #
-release=$(cat VERSION.cmake | grep 'set( ECFLOW_RELEASE' | awk '{print $3}'| sed 's/["]//g')
-major=$(cat VERSION.cmake   | grep 'set( ECFLOW_MAJOR'   | awk '{print $3}'| sed 's/["]//g')
-minor=$(cat VERSION.cmake   | grep 'set( ECFLOW_MINOR'   | awk '{print $3}'| sed 's/["]//g')
+cd $WK
+release=$(cat ACore/src/ecflow_version.h | grep 'ECFLOW_RELEASE' | awk '{print $3}'| sed 's/["]//g')
+major=$(cat ACore/src/ecflow_version.h   | grep 'ECFLOW_MAJOR'   | awk '{print $3}'| sed 's/["]//g')
+minor=$(cat ACore/src/ecflow_version.h   | grep 'ECFLOW_MINOR'   | awk '{print $3}'| sed 's/["]//g')
 
 install_prefix=/var/tmp/$USER/install/cmake/ecflow/$release.$major.$minor
 if [[ $sys_install = sys_install ]] ; then
