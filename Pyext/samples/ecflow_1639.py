6ae04c43a6cb4c6fe536c015e60a09a748538c4d
avi bahra <avi.bahra@ecmwf.int>
2020-05-01 10:41:10 +0100

    ECFLOW-1639 add test

---
+++ Pyext/samples/ecflow_1639.py
@@ -0,0 +1,22 @@
+from ecflow import *
+  
+print ("Simulator::")
+
+
+defs = Defs(
+        Suite("ecflow_1639",
+            Clock(1,5,2020),
+            Task("t1",Day("monday"),Verify(State.complete,0)),
+            Task("t2",Date("1.1.2010"),Verify(State.complete,0)),
+            Task("t3",Time("23:00"),Verify(State.complete,1)),
+            Task("t4",Cron("23:00"),Verify(State.complete,1)),
+            Task("t5",Cron("10:00 20:00 01:00"),Verify(State.complete,11)),
+            )
+        )
+defs.ecflow_1639.add_end_clock(Clock(2,5,2020))
+print(defs)
+
+theResult = defs.simulate()
+assert len(theResult) == 0,  "Expected simulation to return without any errors, but found:\n" + theResult
+
+    
