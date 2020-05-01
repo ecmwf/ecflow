9cf96ae87bc0eb4018a7c2630780293e7c3a559d
avi bahra <avi.bahra@ecmwf.int>
2020-05-01 13:28:28 +0100

    ECFLOW-1638 add test to simulator

--- Pyext/samples/ecflow_1638.py
+++ Pyext/samples/ecflow_1638.py
@@ -1,6 +1,6 @@
 from ecflow import *
   
-print ("Simulator test ecflow_1639")
+print ("Simulator test ecflow_1638")
 
 # the following test fails in ecflow 5.3.1
 # Simulation start    at:: 1,5,2020 @ 00:00
@@ -9,9 +9,8 @@ print ("Simulator test ecflow_1639")
 # Hence we expect  task t4; cron 23:00             to complete 2
 # Hence we expect  task t5; cron 10:00 20:00 01:00 to complete 22
 
-
 defs = Defs(
-        Suite("ecflow_1639",
+        Suite("ecflow_1638",
             Clock(1,5,2020),
             Task("t2",Date("1.5.2010"),Date("1.5.2011"),Verify(State.complete,0)),
             Task("t3",Time("23:00"),Verify(State.complete,1)),
