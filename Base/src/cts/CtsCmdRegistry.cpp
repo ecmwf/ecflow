152894955b1199ed53d243ece4e86179ff43be78
avi bahra <avi.bahra@ecmwf.int>
2020-06-11 14:44:16 +0100

    if registered cmd not found print program options variable map

--- Base/src/cts/CtsCmdRegistry.cpp
+++ Base/src/cts/CtsCmdRegistry.cpp
@@ -110,21 +110,79 @@ CtsCmdRegistry::CtsCmdRegistry(bool addGroupCmd)
 	if (addGroupCmd) vec_.push_back( std::make_shared<GroupCTSCmd>());
 }
 
+
+static void PrintVariableMap(const boost::program_options::variables_map& vm) {
+	std::cout << "boost::program_options::variables_map:    vm.size() " << vm.size() << "\n";
+    for (po::variables_map::const_iterator it = vm.begin(); it != vm.end(); it++) {
+        std::cout << "> " << it->first;
+        if (((boost::any)it->second.value()).empty()) {
+            std::cout << "(empty)";
+        }
+        if (vm[it->first].defaulted() || it->second.defaulted()) {
+            std::cout << "(default)";
+        }
+        std::cout << "=";
+
+        bool is_char;
+        try {
+            boost::any_cast<const char *>(it->second.value());
+            is_char = true;
+        } catch (const boost::bad_any_cast &) {
+            is_char = false;
+        }
+        bool is_str;
+        try {
+            boost::any_cast<std::string>(it->second.value());
+            is_str = true;
+        } catch (const boost::bad_any_cast &) {
+            is_str = false;
+        }
+
+        if (((boost::any)it->second.value()).type() == typeid(int)) {
+            std::cout << vm[it->first].as<int>() << std::endl;
+        } else if (((boost::any)it->second.value()).type() == typeid(bool)) {
+            std::cout << vm[it->first].as<bool>() << std::endl;
+        } else if (((boost::any)it->second.value()).type() == typeid(double)) {
+            std::cout << vm[it->first].as<double>() << std::endl;
+        } else if (is_char) {
+            std::cout << vm[it->first].as<const char * >() << std::endl;
+        } else if (is_str) {
+            std::string temp = vm[it->first].as<std::string>();
+            if (temp.size()) {
+                std::cout << temp << std::endl;
+            } else {
+                std::cout << "true" << std::endl;
+            }
+        } else { // Assumes that the only remainder is vector<string>
+            try {
+                std::vector<std::string> vect = vm[it->first].as<std::vector<std::string> >();
+                size_t i = 0;
+                for (std::vector<std::string>::iterator oit=vect.begin(); oit != vect.end(); oit++, ++i) {
+                    std::cout << "\r> " << it->first << "[" << i << "]=" << (*oit) << std::endl;
+                }
+            } catch (const boost::bad_any_cast &) {
+                std::cout << "UnknownType(" << ((boost::any)it->second.value()).type().name() << ")" << std::endl;
+            }
+        }
+    }
+}
+
 bool CtsCmdRegistry::parse(Cmd_ptr& cmd,
                            boost::program_options::variables_map& vm,
                            AbstractClientEnv* clientEnv ) const
 {
-	size_t vec_size = vec_.size();
-	for(size_t i = 0; i < vec_size; i++) {
+	for(const auto& registered_cmd: vec_) {
 
-		if ( vm.count( vec_[i]->theArg() ) ) {
+		if ( vm.count( registered_cmd->theArg() ) ) {
 
-			if (clientEnv->debug()) std::cout << "  CtsCmdRegistry::parse matched with registered command " << vec_[i]->theArg() << "\n";
+			if (clientEnv->debug()) std::cout << "  CtsCmdRegistry::parse matched with registered command " << registered_cmd->theArg() << "\n";
 
-			vec_[i]->create(cmd,vm,clientEnv);
+			registered_cmd->create(cmd,vm,clientEnv);
 			return true;
 		}
 	}
+
+	PrintVariableMap(vm);
 	return false;
 }
 
@@ -136,9 +194,8 @@ void CtsCmdRegistry::addAllOptions(boost::program_options::options_description& 
 
 void CtsCmdRegistry::addCmdOptions(boost::program_options::options_description& desc) const
 {
-   size_t vec_size = vec_.size();
- 	for(size_t i = 0; i < vec_size; i++) {
-		vec_[i]->addOption(desc);
+	for(const auto& registered_cmd: vec_) {
+		registered_cmd->addOption(desc);
 	}
 }
 
@@ -147,7 +204,7 @@ void CtsCmdRegistry::addHelpOption(boost::program_options::options_description& 
 	/////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
    desc.add_options() ("help,h",po::value< std::string >()->implicit_value( std::string("") ), "Produce help message");
    desc.add_options() ("version,v", "Show ecflow client version number, and version of the boost library used" );
-	desc.add_options() ("debug,d",
+   desc.add_options() ("debug,d",
 			"Dump out client environment settings for debug\n"
 			"Set environment variable ECF_DEBUG_CLIENT for additional debug" );
 }
