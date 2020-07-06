2937c437932e7fdcb9839691c109bdcb95e7e5cf
avi bahra <avi.bahra@ecmwf.int>
2020-07-06 15:11:50 +0100

    ECFLOW-1662 group command fails on the command line

--- Base/src/cts/CtsCmdRegistry.cpp
+++ Base/src/cts/CtsCmdRegistry.cpp
@@ -110,63 +110,6 @@ CtsCmdRegistry::CtsCmdRegistry(bool addGroupCmd)
 	if (addGroupCmd) vec_.push_back( std::make_shared<GroupCTSCmd>());
 }
 
-
-//static void PrintVariableMap(const boost::program_options::variables_map& vm) {
-//	std::cout << "boost::program_options::variables_map:    vm.size() " << vm.size() << "\n";
-//    for (po::variables_map::const_iterator it = vm.begin(); it != vm.end(); it++) {
-//        std::cout << "> " << it->first;
-//        if (((boost::any)it->second.value()).empty()) {
-//            std::cout << "(empty)";
-//        }
-//        if (vm[it->first].defaulted() || it->second.defaulted()) {
-//            std::cout << "(default)";
-//        }
-//        std::cout << "=";
-//
-//        bool is_char;
-//        try {
-//            boost::any_cast<const char *>(it->second.value());
-//            is_char = true;
-//        } catch (const boost::bad_any_cast &) {
-//            is_char = false;
-//        }
-//        bool is_str;
-//        try {
-//            boost::any_cast<std::string>(it->second.value());
-//            is_str = true;
-//        } catch (const boost::bad_any_cast &) {
-//            is_str = false;
-//        }
-//
-//        if (((boost::any)it->second.value()).type() == typeid(int)) {
-//            std::cout << vm[it->first].as<int>() << std::endl;
-//        } else if (((boost::any)it->second.value()).type() == typeid(bool)) {
-//            std::cout << vm[it->first].as<bool>() << std::endl;
-//        } else if (((boost::any)it->second.value()).type() == typeid(double)) {
-//            std::cout << vm[it->first].as<double>() << std::endl;
-//        } else if (is_char) {
-//            std::cout << vm[it->first].as<const char * >() << std::endl;
-//        } else if (is_str) {
-//            std::string temp = vm[it->first].as<std::string>();
-//            if (temp.size()) {
-//                std::cout << temp << std::endl;
-//            } else {
-//                std::cout << "true" << std::endl;
-//            }
-//        } else { // Assumes that the only remainder is vector<string>
-//            try {
-//                std::vector<std::string> vect = vm[it->first].as<std::vector<std::string> >();
-//                size_t i = 0;
-//                for (std::vector<std::string>::iterator oit=vect.begin(); oit != vect.end(); oit++, ++i) {
-//                    std::cout << "\r> " << it->first << "[" << i << "]=" << (*oit) << std::endl;
-//                }
-//            } catch (const boost::bad_any_cast &) {
-//                std::cout << "UnknownType(" << ((boost::any)it->second.value()).type().name() << ")" << std::endl;
-//            }
-//        }
-//    }
-//}
-
 bool CtsCmdRegistry::parse(Cmd_ptr& cmd,
                            boost::program_options::variables_map& vm,
                            AbstractClientEnv* clientEnv ) const
@@ -182,7 +125,6 @@ bool CtsCmdRegistry::parse(Cmd_ptr& cmd,
 		}
 	}
 
-	//PrintVariableMap(vm);
 	return false;
 }
 
