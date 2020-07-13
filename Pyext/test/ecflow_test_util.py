de1fd57b259cac27c9a5ad2712aaa7262a72286a
avi bahra <avi.bahra@ecmwf.int>
2020-07-13 16:41:15 +0100

    remove lock file

--- Pyext/test/ecflow_test_util.py
+++ Pyext/test/ecflow_test_util.py
@@ -245,8 +245,7 @@ class Server(object):
         print("   Terminate server "+ self.at_time() )
         self.ci.terminate_server()  
         print("   Terminate server OK " + self.at_time() )
-        if not debugging():
-            self.lock_file.remove(self.the_port)
+        self.lock_file.remove(self.the_port)
         
         # Do not clean up data, if an assert was raised. This allow debug
         if exctype is None:
