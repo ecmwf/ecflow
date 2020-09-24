060a6cc0d8c1cefa000683b7d6d435bd9158aeb1
Avi Bahra <Avi.Bahra@ecmwf.int>
2020-09-24 08:11:13 +0100

    Revert "Update to next release"
    This reverts commit e329c9107fe7aeffc70ca1d838432a6907270236.

--- ACore/src/ecflow_version.h
+++ ACore/src/ecflow_version.h
@@ -1,14 +1,14 @@
 #ifndef ecflow_version_config_h
 #define ecflow_version_config_h
  
-#define ECFLOW_VERSION   "5.5.4"
+#define ECFLOW_VERSION   "5.5.3"
 #define ECFLOW_RELEASE   "5"
 #define ECFLOW_MAJOR     "5"
-#define ECFLOW_MINOR     "4"
+#define ECFLOW_MINOR     "3"
 
 // available but not used
-//PROJECT_VERSION=5.5.4
+//PROJECT_VERSION=5.5.3
 //PROJECT_VERSION_MAJOR=5
 //PROJECT_VERSION_MINOR=5
-//PROJECT_VERSION_PATCH=4
+//PROJECT_VERSION_PATCH=3
 #endif 
