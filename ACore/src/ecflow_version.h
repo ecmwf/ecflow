088a9b084549067385b322e609c33cefaf927d31
avi bahra <avi.bahra@ecmwf.int>
2020-11-11 11:21:34 +0000

    Update version to 5.6.0

--- ACore/src/ecflow_version.h
+++ ACore/src/ecflow_version.h
@@ -1,14 +1,14 @@
 #ifndef ecflow_version_config_h
 #define ecflow_version_config_h
  
-#define ECFLOW_VERSION   "5.5.4"
+#define ECFLOW_VERSION   "5.6.0"
 #define ECFLOW_RELEASE   "5"
-#define ECFLOW_MAJOR     "5"
-#define ECFLOW_MINOR     "4"
+#define ECFLOW_MAJOR     "6"
+#define ECFLOW_MINOR     "0"
 
 // available but not used
-//PROJECT_VERSION=5.5.4
+//PROJECT_VERSION=5.6.0
 //PROJECT_VERSION_MAJOR=5
-//PROJECT_VERSION_MINOR=5
-//PROJECT_VERSION_PATCH=4
+//PROJECT_VERSION_MINOR=6
+//PROJECT_VERSION_PATCH=0
 #endif 
