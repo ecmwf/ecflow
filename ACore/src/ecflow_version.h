5dcaf4d034de2005b00961162bb0372340bc8d0f
Shahram Najm <masn@ecmwf.int>
2021-04-06 17:12:59 +0100

    Update version to 5.7.0

--- ACore/src/ecflow_version.h
+++ ACore/src/ecflow_version.h
@@ -1,15 +1,15 @@
 #ifndef ecflow_version_config_h
 #define ecflow_version_config_h
  
-#define ECFLOW_VERSION   "5.6.1"
+#define ECFLOW_VERSION   "5.7.0"
 #define ECFLOW_RELEASE   "5"
-#define ECFLOW_MAJOR     "6"
-#define ECFLOW_MINOR     "1"
+#define ECFLOW_MAJOR     "7"
+#define ECFLOW_MINOR     "0"
 
 // available but not used
-//PROJECT_VERSION=5.6.1
+//PROJECT_VERSION=5.7.0
 //PROJECT_VERSION_MAJOR=5
-//PROJECT_VERSION_MINOR=6
-//PROJECT_VERSION_PATCH=1
+//PROJECT_VERSION_MINOR=7
+//PROJECT_VERSION_PATCH=0
 
 #endif 
