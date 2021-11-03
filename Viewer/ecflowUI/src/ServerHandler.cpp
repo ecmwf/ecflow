330159b52414feb00dc0adcd94628e72f76f32b8
Sandor Kertesz <cgr@ecmwf.int>
2021-11-03 17:45:01 +0000

    ecFlowUI: node status is not updated automatically in tree view with Qt6 ECFLOW-1760

--- Viewer/ecflowUI/src/ServerHandler.cpp
+++ Viewer/ecflowUI/src/ServerHandler.cpp
@@ -13,6 +13,7 @@
 #include "ClientInvoker.hpp"
 #include "File.hpp"
 #include "NodeFwd.hpp"
+#include "Node.hpp"
 #include "ArgvCreator.hpp"
 #include "Str.hpp"
 
@@ -98,6 +99,8 @@ ServerHandler::ServerHandler(const std::string& name,const std::string& host, co
         qRegisterMetaType<std::string>("std::string");
         qRegisterMetaType<QList<ecf::Aspect::Type> >("QList<ecf::Aspect::Type>");
         qRegisterMetaType<std::vector<ecf::Aspect::Type> >("std::vector<ecf::Aspect::Type>");
+        // to do this we need to incluce Node.hpp !
+        qRegisterMetaType<const Node*>("const Node*");
     }
 
     //Add this instance to the servers_ list.
