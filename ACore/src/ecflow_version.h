dfd0636e1022ad32ad4364e4e850f6fb68db905f
Iain Russell <Iain.Russell@ecmwf.int>
2022-03-08 09:25:10 +0000

    Update version to 5.8.3

--- ACore/src/ecflow_version.h
+++ ACore/src/ecflow_version.h
@@ -1,15 +1,15 @@
 #ifndef ecflow_version_config_h
 #define ecflow_version_config_h
  
-#define ECFLOW_VERSION   "5.8.2"
+#define ECFLOW_VERSION   "5.8.3"
 #define ECFLOW_RELEASE   "5"
 #define ECFLOW_MAJOR     "8"
-#define ECFLOW_MINOR     "2"
+#define ECFLOW_MINOR     "3"
 
 // available but not used
-//PROJECT_VERSION=5.8.2
+//PROJECT_VERSION=5.8.3
 //PROJECT_VERSION_MAJOR=5
 //PROJECT_VERSION_MINOR=8
-//PROJECT_VERSION_PATCH=2
+//PROJECT_VERSION_PATCH=3
 
 #endif 
