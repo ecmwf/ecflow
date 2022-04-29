676625e8b7ae5dd4a1de2a955c51d7edcf2346d4
Shahram Najm <masn@ecmwf.int>
2022-04-29 12:18:51 +0100

    Update version to 5.8.4

--- ACore/src/ecflow_version.h
+++ ACore/src/ecflow_version.h
@@ -1,15 +1,15 @@
 #ifndef ecflow_version_config_h
 #define ecflow_version_config_h
  
-#define ECFLOW_VERSION   "5.8.3"
+#define ECFLOW_VERSION   "5.8.4"
 #define ECFLOW_RELEASE   "5"
 #define ECFLOW_MAJOR     "8"
-#define ECFLOW_MINOR     "3"
+#define ECFLOW_MINOR     "4"
 
 // available but not used
-//PROJECT_VERSION=5.8.3
+//PROJECT_VERSION=5.8.4
 //PROJECT_VERSION_MAJOR=5
 //PROJECT_VERSION_MINOR=8
-//PROJECT_VERSION_PATCH=3
+//PROJECT_VERSION_PATCH=4
 
 #endif 
