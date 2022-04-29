ea4b89399d1e7b897ff48c59b1e885e6d53cc8d6
Shahram Najm <masn@ecmwf.int>
2022-04-29 12:18:51 +0100

    Update version to 5.8.4

--- Pyext/ecflow/__init__.py
+++ Pyext/ecflow/__init__.py
@@ -15,6 +15,6 @@ from .ecflow import *
 The ecFlow python module
 """
 
-__version__ = '5.8.3'
+__version__ = '5.8.4'
 
 # http://stackoverflow.com/questions/13040646/how-do-i-create-documentation-with-pydoc
