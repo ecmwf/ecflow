52a90fb311a82b6fe4e89be643914fec4d032685
Sandor Kertesz <Sandor.Kertesz@ecmwf.int>
2022-11-08 12:50:37 +0000

    Set version number for hotfix/5.9.1

--- ACore/src/ecflow_version.h
+++ ACore/src/ecflow_version.h
@@ -1,15 +1,15 @@
 #ifndef ecflow_version_config_h
 #define ecflow_version_config_h
  
-#define ECFLOW_VERSION   "5.9.0"
+#define ECFLOW_VERSION   "5.9.1"
 #define ECFLOW_RELEASE   "5"
 #define ECFLOW_MAJOR     "9"
-#define ECFLOW_MINOR     "0"
+#define ECFLOW_MINOR     "1"
 
 // available but not used
-//PROJECT_VERSION=5.9.0
+//PROJECT_VERSION=5.9.1
 //PROJECT_VERSION_MAJOR=5
 //PROJECT_VERSION_MINOR=9
-//PROJECT_VERSION_PATCH=0
+//PROJECT_VERSION_PATCH=1
 
 #endif 
