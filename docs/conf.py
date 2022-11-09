aa6f00bd50b3696f0325763641ab04eb0bdede91
Sandor Kertesz <Sandor.Kertesz@ecmwf.int>
2022-11-09 12:09:06 +0000

    Move documentation to Read the Docs ECFLOW-1846

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ copyright = "%s, European Centre for Medium-Range Weather Forecasts (ECMWF)" % (
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx_rtd_theme", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
+extensions = ["sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
