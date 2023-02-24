d6ae8acf0132d3230925f1d854b431e50231a941
Iain Russell <Iain.Russell@ecmwf.int>
2023-02-24 15:54:41 +0000

    Update version number to 5.10.81 (for testing)

--- ACore/src/ecflow_version.h
+++ ACore/src/ecflow_version.h
@@ -2,15 +2,15 @@
 #define ecflow_version_config_h
 
 // clang-format off
-#define ECFLOW_VERSION   "5.10.80"
+#define ECFLOW_VERSION   "5.10.81"
 #define ECFLOW_RELEASE   "5"
 #define ECFLOW_MAJOR     "10"
-#define ECFLOW_MINOR     "80"
+#define ECFLOW_MINOR     "81"
 
 // available but not used
-// PROJECT_VERSION=5.10.80
+// PROJECT_VERSION=5.10.81
 // PROJECT_VERSION_MAJOR=5
 // PROJECT_VERSION_MINOR=10
-// PROJECT_VERSION_PATCH=80
+// PROJECT_VERSION_PATCH=81
 
 #endif 
