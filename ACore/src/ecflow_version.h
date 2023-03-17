861707e84aa7eac296388b666154c3245927d940
Marcos Bento <Marcos.Bento@ecmwf.int>
2023-03-17 14:47:21 +0100

    Update version number to 5.10.82 (for testing)

--- ACore/src/ecflow_version.h
+++ ACore/src/ecflow_version.h
@@ -2,15 +2,15 @@
 #define ecflow_version_config_h
 
 // clang-format off
-#define ECFLOW_VERSION   "5.10.81"
+#define ECFLOW_VERSION   "5.10.82"
 #define ECFLOW_RELEASE   "5"
 #define ECFLOW_MAJOR     "10"
-#define ECFLOW_MINOR     "81"
+#define ECFLOW_MINOR     "82"
 
 // available but not used
-// PROJECT_VERSION=5.10.81
+// PROJECT_VERSION=5.10.82
 // PROJECT_VERSION_MAJOR=5
 // PROJECT_VERSION_MINOR=10
-// PROJECT_VERSION_PATCH=81
+// PROJECT_VERSION_PATCH=82
 
 #endif 
