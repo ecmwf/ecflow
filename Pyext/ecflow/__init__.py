7e69e8a034a3c2dc64c15485c7aeeb5e863e6aee
Marcos Bento <Marcos.Bento@ecmwf.int>
2023-04-04 13:35:04 +0200

    Update version number to 5.10.84 (for testing)

--- Pyext/ecflow/__init__.py
+++ Pyext/ecflow/__init__.py
@@ -15,6 +15,6 @@ from .ecflow import *
 The ecFlow python module
 """
 
-__version__ = '5.10.83'
+__version__ = '5.10.84'
 
 # http://stackoverflow.com/questions/13040646/how-do-i-create-documentation-with-pydoc
