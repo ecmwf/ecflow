2c3528a1d3165000c246acf4ec0ee6573726a199
Marcos Bento <Marcos.Bento@ecmwf.int>
2023-05-30 18:28:55 +0200

    Use MSG_NOSIGNAL only when defined ECFLOW-1881

--- Http/src/ApiV1.hpp
+++ Http/src/ApiV1.hpp
@@ -17,8 +17,17 @@
 //
 /////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
 
+#include <sys/socket.h>
+#if defined(MSG_NOSIGNAL)
+// The MSG_NOSIGNAL flag should be made defined by <sys/socket.h>
+// (as per POSIX: https://pubs.opengroup.org/onlinepubs/9699919799.2018edition/functions/send.html),
+// but that is not always the case for OSX.
+//
+// If MSG_NOSIGNAL is not defined, we leave it up to httplib to use default flags.
+//
 #define CPPHTTPLIB_SEND_FLAGS MSG_NOSIGNAL
 #define CPPHTTPLIB_RECV_FLAGS MSG_NOSIGNAL
+#endif
 
 #ifdef ECF_OPENSSL
     #define CPPHTTPLIB_OPENSSL_SUPPORT
