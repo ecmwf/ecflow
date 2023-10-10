41f046777710a6eecdf1ea02213ecf644ca278d7
Iain Russell <Iain.Russell@ecmwf.int>
2023-10-10 14:18:14 +0000

    Bump version to 5.11.4

--- ACore/src/ecflow_version.h
+++ ACore/src/ecflow_version.h
@@ -2,15 +2,15 @@
 #define ecflow_version_config_h
 
 // clang-format off
-#define ECFLOW_VERSION   "5.11.3"
+#define ECFLOW_VERSION   "5.11.4"
 #define ECFLOW_RELEASE   "5"
 #define ECFLOW_MAJOR     "11"
-#define ECFLOW_MINOR     "3"
+#define ECFLOW_MINOR     "4"
 
 // available but not used
-// PROJECT_VERSION=5.11.3
+// PROJECT_VERSION=5.11.4
 // PROJECT_VERSION_MAJOR=5
 // PROJECT_VERSION_MINOR=11
-// PROJECT_VERSION_PATCH=3
+// PROJECT_VERSION_PATCH=4
 
 #endif 
