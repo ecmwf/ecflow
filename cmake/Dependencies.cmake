9fb471a833c864e9ace206c0fc40255bd8af6ab8
Marcos Bento <Marcos.Bento@ecmwf.int>
2023-11-01 14:42:00 +0000

    deps: add cmake Find<>.cmake modules for 3rd party

---
+++ cmake/Dependencies.cmake
@@ -0,0 +1,28 @@
+#
+# Copyright 2023- ECMWF.
+# This software is licensed under the terms of the Apache Licence version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+#
+
+set(DEPENDENCIES_DIR "${CMAKE_SOURCE_DIR}/3rdparty")
+
+# =========================================================================================
+# Cereal
+# =========================================================================================
+set(CEREAL_DIR "${DEPENDENCIES_DIR}/cereal")
+find_package(Cereal REQUIRED)
+
+# =========================================================================================
+# Json
+# =========================================================================================
+set(JSON_DIR "${DEPENDENCIES_DIR}/json")
+find_package(Json REQUIRED)
+
+# =========================================================================================
+# Httplib
+# =========================================================================================
+set(HTTPLIB_DIR "${DEPENDENCIES_DIR}/cpp-httplib")
+find_package(Httplib REQUIRED)
