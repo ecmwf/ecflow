9794e5d9ca7fae3fb65d3d4b443023edfef1e33d
Marcos Bento <Marcos.Bento@ecmwf.int>
2023-11-30 15:09:31 +0000

    Avoid ecFlow UDP test failure

--- Udp/test/TestSupport.hpp
+++ Udp/test/TestSupport.hpp
@@ -240,7 +240,7 @@ public:
         bp::child server(invoke_command);
 
         // Wait for server to start...
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        std::this_thread::sleep_for(std::chrono::milliseconds(2000));
 
         return server;
     }
