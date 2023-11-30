0df5efb1efaad228407f58409c47ba636055c670
Marcos Bento <Marcos.Bento@ecmwf.int>
2023-11-30 09:26:42 +0000

    Use ECMWF standard header on ecflow/python source code
    Re ECFLOW-1932

--- Pyext/src/Edit.hpp
+++ Pyext/src/Edit.hpp
@@ -1,19 +1,15 @@
-#ifndef EDIT_HPP_
-#define EDIT_HPP_
-/////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
-// Name        :
-// Author      : Avi
-// Revision    : $Revision: #18 $
-//
-// Copyright 2009- ECMWF.
-// This software is licensed under the terms of the Apache Licence version 2.0
-// which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
-// In applying this licence, ECMWF does not waive the privileges and immunities
-// granted to it by virtue of its status as an intergovernmental organisation
-// nor does it submit to any jurisdiction.
-//
-// Description :
-/////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
+/*
+ * Copyright 2009- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation
+ * nor does it submit to any jurisdiction.
+ */
+
+#ifndef ecflow_python_Edit_HPP
+#define ecflow_python_Edit_HPP
 
 #include <vector>
 
@@ -33,4 +29,4 @@ private:
     std::vector<Variable> vec_;
 };
 
-#endif
+#endif /* ecflow_python_Edit_HPP */
