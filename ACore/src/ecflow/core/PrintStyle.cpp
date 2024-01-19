492c2e44448b6db72e09a6a41c2eb1acee8eaec3
Marcos Bento <Marcos.Bento@ecmwf.int>
2024-01-19 17:12:40 +0000

    Use Enumerate<PrintStyle::Type_t>

--- ACore/src/ecflow/core/PrintStyle.cpp
+++ ACore/src/ecflow/core/PrintStyle.cpp
@@ -10,6 +10,31 @@
 
 #include "ecflow/core/PrintStyle.hpp"
 
+#include "ecflow/core/Enumerate.hpp"
+
+namespace ecf::detail {
+
+template <>
+struct EnumTraits<PrintStyle::Type_t>
+{
+    using underlying_t = std::underlying_type_t<PrintStyle::Type_t>;
+
+    static constexpr std::array map = std::array{
+        // clang-format off
+        std::make_pair(PrintStyle::Type_t::NOTHING, "NOTHING"),
+        std::make_pair(PrintStyle::Type_t::DEFS, "DEFS"),
+        std::make_pair(PrintStyle::Type_t::STATE, "STATE"),
+        std::make_pair(PrintStyle::Type_t::MIGRATE, "MIGRATE"),
+        std::make_pair(PrintStyle::Type_t::NET, "NET"),
+        // clang-format on
+    };
+    static constexpr size_t size = map.size();
+
+    static_assert(EnumTraits<PrintStyle::Type_t>::size == map.back().first + 1);
+};
+
+} // namespace ecf::detail
+
 static PrintStyle::Type_t style_ = PrintStyle::NOTHING;
 
 PrintStyle::Type_t PrintStyle::getStyle() {
@@ -46,22 +71,5 @@ std::string PrintStyle::to_string() {
 }
 
 std::string PrintStyle::to_string(PrintStyle::Type_t t) {
-    switch (t) {
-        case PrintStyle::NOTHING:
-            return "NOTHING";
-            break;
-        case PrintStyle::DEFS:
-            return "DEFS";
-            break;
-        case PrintStyle::STATE:
-            return "STATE";
-            break;
-        case PrintStyle::MIGRATE:
-            return "MIGRATE";
-            break;
-        case PrintStyle::NET:
-            return "NET";
-            break;
-    }
-    return std::string();
+    return std::string{ecf::Enumerate<PrintStyle::Type_t>::to_string(t).value_or("")};
 }
