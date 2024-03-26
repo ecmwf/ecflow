1f4bc917ea815704d699c662362560f746fabaea
Marcos Bento <Marcos.Bento@ecmwf.int>
2024-03-26 16:38:53 +0000

    Silence compilation warning [-Wunused-variable]

--- libs/rest/src/ecflow/http/ApiV1.cpp
+++ libs/rest/src/ecflow/http/ApiV1.cpp
@@ -176,7 +176,7 @@ ojson filter_json(const ojson& j, const httplib::Request& r) {
     // special case: filter is .[INDEX], means that we return the
     // correct array element from root json element assuming it's an array
     if (path_elems.size() == 1 && path_elems[0][0] == '[' && path_elems[0][path_elems[0].size() - 1] == ']') {
-        const auto [key, index] = get_array_info(path_elems[0]);
+        [[maybe_unused]] const auto [key, index] = get_array_info(path_elems[0]); // n.b. only key is unused
         return j[index];
     }
 
