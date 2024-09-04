e973550d0577913cfc947ccc995a9424459fc594
Marcos Bento <Marcos.Bento@ecmwf.int>
2024-09-04 16:32:14 +0100

    Enable REST/UDP proxies to use HTTP connection
    Also, fix an eventual race condition accessing:
     - Ecf::state_change_no_ and Ecf::state_modify_no_
    Apart from making these global (instead of thread_local),
    the Http library is restricted to the use of a single thread
    
    Re ECFLOW-1957

--- libs/server/src/ecflow/server/Server.hpp
+++ libs/server/src/ecflow/server/Server.hpp
@@ -11,11 +11,10 @@
 #ifndef ecflow_server_Server_HPP
 #define ecflow_server_Server_HPP
 
-#include <httplib.h>
-
 #include "ecflow/base/stc/PreAllocatedReply.hpp"
 #include "ecflow/core/Converter.hpp"
 #include "ecflow/server/BaseServer.hpp"
+#include "ecflow/server/HttpServer.hpp"
 #include "ecflow/server/ServerEnvironment.hpp"
 #include "ecflow/server/SslTcpServer.hpp"
 #include "ecflow/server/TcpServer.hpp"
@@ -42,67 +41,8 @@ private:
     U server_;
 };
 
-using Server    = DefaultServer<TcpServer>;
-using SslServer = DefaultServer<SslTcpServer>;
-
-class HttpServer : public BaseServer {
-public:
-    explicit HttpServer(boost::asio::io_service& service, ServerEnvironment& env) : BaseServer(service, env) {}
-    ~HttpServer() override = default;
-
-    std::string ssl() const override { return ""; }
-
-    void run() {
-        httplib::Server server;
-
-        server.Post("/v1/ecflow", [this](const httplib::Request& request, httplib::Response& response) {
-            // Buffers to hold request/response
-            ClientToServerRequest inbound_request;
-            ServerToClientResponse outbound_response;
-
-            // 1) unserialize request body into inbound_request
-            ecf::restore_from_string(request.body, inbound_request);
-
-            // 2) handle request, as per TcpBaseServer::handle_request()
-            {
-                // See what kind of message we got from the client
-                if (serverEnv_.debug()) {
-                    std::cout << "   TcpBaseServer::handle_request  : client request " << inbound_request << std::endl;
-                }
-
-                try {
-                    // Service the in bound request, handling the request will populate the outbound_response_
-                    // Note:: Handle request will first authenticate
-                    outbound_response.set_cmd(inbound_request.handleRequest(this));
-                }
-                catch (std::exception& e) {
-                    outbound_response.set_cmd(PreAllocatedReply::error_cmd(e.what()));
-                }
-
-                // Do any necessary clean up after inbound_request_ has run. i.e like re-claiming memory
-                inbound_request.cleanup();
-            }
-
-            // 3) serialize outbound_response into response body
-            std::string outbound;
-            ecf::save_as_string(outbound, outbound_response);
-
-            // 4) ship response
-            response.set_content(outbound, "text/plain");
-        });
-
-        try {
-            auto [host, port] = serverEnv_.hostPort();
-
-            bool ret = server.listen(host, ecf::convert_to<int>(port));
-            if (ret == false) {
-                throw std::runtime_error(std::string("Failed to start HTTP server"));
-            }
-        }
-        catch (const std::exception& e) {
-            std::cout << "[HttpServer] Failure running HTTP server:" << e.what() << std::endl;
-        }
-    }
-};
+using Server     = DefaultServer<TcpServer>;
+using SslServer  = DefaultServer<SslTcpServer>;
+using HttpServer = DefaultServer<HttpListener>;
 
 #endif /* ecflow_server_Server_HPP */
