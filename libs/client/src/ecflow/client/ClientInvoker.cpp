db7fc9371e2f4274c66f4fcc2e3fac774b0db200
Marcos Bento <Marcos.Bento@ecmwf.int>
2024-10-02 13:22:06 +0100

    Enable ecFlow REST API tests using HTTP backend
    Re ECFLOW-1957

--- libs/client/src/ecflow/client/ClientInvoker.cpp
+++ libs/client/src/ecflow/client/ClientInvoker.cpp
@@ -421,8 +421,9 @@ int ClientInvoker::do_invoke_cmd(Cmd_ptr cts_cmd) const {
     #endif
                             io.run();
                         }
-                        if (clientEnv_.debug())
+                        if (clientEnv_.debug()) {
                             cout << TimeStamp::now() << "ClientInvoker: >>> After: io_context::run() <<<" << endl;
+                        }
 
                         /// Let see how the server responded if at all.
                         try {
@@ -441,8 +442,9 @@ int ClientInvoker::do_invoke_cmd(Cmd_ptr cts_cmd) const {
                         HttpClient theClient(cts_cmd, clientEnv_.host(), clientEnv_.port());
                         theClient.run();
 
-                        if (clientEnv_.debug())
+                        if (clientEnv_.debug()) {
                             cout << TimeStamp::now() << "ClientInvoker: >>> After: io_service.run() <<<" << endl;
+                        }
 
                         /// Let see how the server responded if at all.
                         try {
@@ -467,8 +469,9 @@ int ClientInvoker::do_invoke_cmd(Cmd_ptr cts_cmd) const {
 #endif
                             io.run();
                         }
-                        if (clientEnv_.debug())
+                        if (clientEnv_.debug()) {
                             cout << TimeStamp::now() << "ClientInvoker: >>> After: io_context::run() <<<" << endl;
+                        }
 
                         /// Let see how the server responded if at all.
                         try {
