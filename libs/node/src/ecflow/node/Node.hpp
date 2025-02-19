279d84d73d74b51651323b1d3a77ca4757a5c8dc
Marcos Bento <Marcos.Bento@ecmwf.int>
2025-02-19 11:30:38 +0000

    Add algorithm to visit Server/Defs/Nodes on a path
    This algorithm allows visiting the nodes on a given path and to collect
    the applicable per node permission used for Authorisation
    
    Re ECFLOW-1960

--- libs/node/src/ecflow/node/Node.hpp
+++ libs/node/src/ecflow/node/Node.hpp
@@ -61,6 +61,21 @@ class AvisoAttr;
 class MirrorAttr;
 } // namespace ecf
 
+class Node;
+
+struct Permissions
+{
+    explicit Permissions(const Node* node) : node_{node} {}
+
+    [[nodiscard]] bool allows(const std::string& user) const {
+        std::cout << "check if user '" << user << "' is allowed" << std::endl;
+        return user == "mamb";
+    }
+
+private:
+    [[maybe_unused]] const Node* const node_;
+};
+
 class Node : public std::enable_shared_from_this<Node> {
 protected:
     Node(const std::string& name, bool check);
@@ -83,6 +98,8 @@ public:
     void set_parent(Node* p) { parent_ = p; }
 
     virtual node_ptr clone() const = 0;
+
+    const Permissions& permissions() const { return permissions_; }
 
     // Server called functions:
     //   Required when we have time attributes, when time related attribute are free they stay free, until re-queue
@@ -953,6 +970,8 @@ private:
 #endif
 
     bool suspended_{false};
+
+    mutable Permissions permissions_{this};
 
     friend class MiscAttrs;
 
