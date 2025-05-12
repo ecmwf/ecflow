b7519056bfa6a01af7941234aeb1f547aab2a41d
Marcos Bento <Marcos.Bento@ecmwf.int>
2025-05-12 09:10:21 +0100

    Correct Permissions selection algorithm
    Based on the latest discussion, the permissions selection algorithm
    should only permit restraining allowances when navigating the node tree
    from the server to the leaves (e.g. a user that starts by having 'rw'
    permissions, can in a lower node be allowed only 'r' but never will he
    be allowed to 'rwx').
    
    Re ECFLOW-1960

--- libs/node/src/ecflow/node/Permissions.hpp
+++ libs/node/src/ecflow/node/Permissions.hpp
@@ -105,11 +105,87 @@ public:
         return found != std::end(allowed_);
     }
 
+    Permissions supersede(const Permissions& other) const {
+        // `*this` is the new permissions
+        // `other` is the previous permissions
+        // We want to update previous permissions, by restricting them using the new permissions
+
+        std::vector<Permission> result;
+
+        for (auto&& new_permission : this->allowed_) {
+            if (auto found =
+                    std::find_if(std::begin(other.allowed_),
+                                 std::end(other.allowed_),
+                                 [&](auto&& current) { return current.username() == new_permission.username(); });
+                found != std::end(other.allowed_)) {
+                result.push_back(Permission{new_permission.username(), new_permission.allowed() & found->allowed()});
+            }
+        }
+
+        return Permissions{std::move(result)};
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Permissions& p) {
+        using namespace std::string_literals;
+        os << "Permissions: "s;
+        for (auto&& permission : p.allowed_) {
+            os << " --> "s << permission.username().value() << ":"s << to_string(permission.allowed()) << " "s;
+        }
+        return os;
+    }
+
 private:
     Permissions() : allowed_{} {}
     explicit Permissions(std::vector<Permission> allowed) : allowed_{std::move(allowed)} {}
 
     std::vector<Permission> allowed_;
+};
+
+class ActivePermissions {
+public:
+    static ActivePermissions make_empty() { return ActivePermissions(); }
+
+    [[nodiscard]] bool is_none() const { return server_permissions_.is_empty() && node_permissions_.is_empty(); }
+
+    [[nodiscard]] bool allows(const Username& username, Allowed permission) const {
+        if (is_none()) {
+            return true; // no active rules, so allow everything
+        }
+
+        if (!server_permissions_.is_empty() && server_permissions_.allows(username, permission)) {
+            return true;
+        }
+
+        if (!node_permissions_.is_empty() && node_permissions_.allows(username, permission)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    void bootstrap_server_permission(const Permissions& p) { server_permissions_ = p; }
+
+    void bootstrap_node_permission(const Permissions& p) { node_permissions_ = p; }
+
+    void combine_node_permission(const Permissions& p) {
+        if (!p.is_empty()) {
+            node_permissions_ = p.supersede(node_permissions_);
+        }
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const ActivePermissions& p) {
+        using namespace std::string_literals;
+        os << "ActivePermissions\n"s;
+        os << " -- Server\n"s;
+        os << p.server_permissions_;
+        os << "\n -- Node\n"s;
+        os << p.node_permissions_;
+        return os;
+    }
+
+private:
+    Permissions server_permissions_ = Permissions::make_empty();
+    Permissions node_permissions_   = Permissions::make_empty();
 };
 
 } // namespace ecf
