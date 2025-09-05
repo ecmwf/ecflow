66132429badfa37ec20ddf2a8ffb85e0273debbb
Marcos Bento <Marcos.Bento@ecmwf.int>
2025-09-05 09:48:15 +0100

    Correct test assert messages
    Re ECFLOW-2041

--- libs/rest/test/InvokeServer.hpp
+++ libs/rest/test/InvokeServer.hpp
@@ -38,8 +38,9 @@ public:
 
         std::string theServerInvokePath = ecf::File::find_ecf_server_path();
 
-        BOOST_REQUIRE_MESSAGE(!theServerInvokePath.empty(), "The server program was found");
-        BOOST_REQUIRE_MESSAGE(fs::exists(theServerInvokePath), "Server executable exist at:" << theServerInvokePath);
+        BOOST_REQUIRE_MESSAGE(!theServerInvokePath.empty(), "The server executable could not found");
+        BOOST_REQUIRE_MESSAGE(fs::exists(theServerInvokePath),
+                              "The server executable does not exist at: " << theServerInvokePath);
 
         BOOST_TEST_MESSAGE("Using eclow_server from " << theServerInvokePath);
 
