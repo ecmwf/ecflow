c8aca039642c42e08dc37ee85e33160d802057f0
Marcos Bento <Marcos.Bento@ecmwf.int>
2025-10-06 09:52:39 +0100

    Update Tutorial 'Load the suite'

--- docs/tutorial/getting_started/src/load-the-file.py
+++ docs/tutorial/getting_started/src/load-the-file.py
@@ -1,8 +1,18 @@
-import ecflow
+#!/usr/bin/env python3
 
-try:
-    print("Loading definition in 'test.def' into the server")
-    ci = ecflow.Client()
-    ci.load("test.def")  # read definition from disk and load into the server
-except RuntimeError as e:
-    print("Failed:", e)
+import pathlib
+from ecflow import Client
+
+if __name__ == '__main__':
+
+    base = pathlib.Path.home() / "course"
+
+    try:
+        ci = Client()
+
+        print("[1] Load suite definition in 'test.def' into the server")
+        ci.load(str(base / "test.def"))
+        # line above reads suite definition from disk and loads into the server
+
+    except RuntimeError as e:
+        print("Failed:", e)
