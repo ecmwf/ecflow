deda34daa962c572bb61bcea55e5e56a035cae76
Marcos Bento <Marcos.Bento@ecmwf.int>
2026-07-08 16:02:37 +0100

    Convert python unit tests to pytest
    Re ECFLOW-2110

--- libs/pyext/test/py_u_TestCronConstructor.py
+++ libs/pyext/test/py_u_TestCronConstructor.py
@@ -8,14 +8,10 @@
 # nor does it submit to any jurisdiction.
 #
 
-import os
 import ecflow as ecf
-import itertools as it
-
-import ecflow_test_util as Test
 
 
-def can_create_cron_from_default_parameters():
+def test_create_cron_from_default_parameters():
     cron = ecf.Cron()
 
     cron.set_days_of_month([10, 20, 30])
@@ -27,69 +23,76 @@ def can_create_cron_from_default_parameters():
     assert cron.months == [1, 2, 3, 4, 5, 6]
 
 
-def can_create_cron_that_runs_every_day_at_2pm():
-    cron = ecf.Cron('14:00')
+def test_create_cron_that_runs_every_day_at_2pm():
+    cron = ecf.Cron("14:00")
     assert cron.time() == ecf.TimeSeries(14, 00)
 
 
-def can_create_cron_that_runs_every_30_minutes_between_0am_and_8pm_on_the_first_six_days_of_the_months_from_january_until_july():
-    cron = ecf.Cron('+00:00 20:00 00:30', days_of_month=[1, 2, 3, 4, 5, 6], months=[1, 2, 3, 4, 5, 6])
+def test_create_cron_that_runs_every_30_minutes_between_0am_and_8pm_on_the_first_six_days_of_the_months_from_january_until_july():
+    cron = ecf.Cron(
+        "+00:00 20:00 00:30",
+        days_of_month=[1, 2, 3, 4, 5, 6],
+        months=[1, 2, 3, 4, 5, 6],
+    )
 
-    assert cron.time() == ecf.TimeSeries(ecf.TimeSlot(0, 0), ecf.TimeSlot(20, 0), ecf.TimeSlot(0, 30), True)
+    assert cron.time() == ecf.TimeSeries(
+        ecf.TimeSlot(0, 0), ecf.TimeSlot(20, 0), ecf.TimeSlot(0, 30), True
+    )
     assert cron.days_of_month == [1, 2, 3, 4, 5, 6]
     assert cron.months == [1, 2, 3, 4, 5, 6]
 
 
-def can_create_cron_that_runs_relative_to_suite_start_time_or_task_requeue():
-    cron = ecf.Cron('+00:15 23:00 00:30', days_of_week=[0, 1, 2], days_of_month=[4, 5, 6], months=[1, 2, 3])
+def test_create_cron_relative_to_suite_start_time_or_task_requeue():
+    cron = ecf.Cron(
+        "+00:15 23:00 00:30",
+        days_of_week=[0, 1, 2],
+        days_of_month=[4, 5, 6],
+        months=[1, 2, 3],
+    )
 
-    assert cron.time() == ecf.TimeSeries(ecf.TimeSlot(0, 15), ecf.TimeSlot(23, 0), ecf.TimeSlot(0, 30), True)
+    assert cron.time() == ecf.TimeSeries(
+        ecf.TimeSlot(0, 15), ecf.TimeSlot(23, 0), ecf.TimeSlot(0, 30), True
+    )
     assert cron.week_days == [0, 1, 2]
     assert cron.days_of_month == [4, 5, 6]
     assert cron.months == [1, 2, 3]
 
-def can_create_cron_based_on_time_series():
-    bgn = ecf.TimeSlot(0 , 0)
+
+def test_create_cron_based_on_time_series():
+    bgn = ecf.TimeSlot(0, 0)
     end = ecf.TimeSlot(23, 0)
     inc = ecf.TimeSlot(0, 30)
     ts = ecf.TimeSeries(bgn, end, inc, True)
 
-    cron = ecf.Cron(ts, days_of_week=[0,1,2,3,4,5,6],days_of_month=[1,2,3,4,5,6], months=[1,2])
+    cron = ecf.Cron(
+        ts,
+        days_of_week=[0, 1, 2, 3, 4, 5, 6],
+        days_of_month=[1, 2, 3, 4, 5, 6],
+        months=[1, 2],
+    )
 
     assert cron.time() == ts
-    assert cron.week_days == [0,1,2,3,4,5,6]
-    assert cron.days_of_month == [1,2,3,4,5,6]
-    assert cron.months == [1,2]
+    assert cron.week_days == [0, 1, 2, 3, 4, 5, 6]
+    assert cron.days_of_month == [1, 2, 3, 4, 5, 6]
+    assert cron.months == [1, 2]
 
-def can_create_cron_with_defaults_and_set_time_series():
+
+def test_create_cron_with_defaults_and_set_time_series():
     cron = ecf.Cron()
     cron.set_time_series(1, 30, True)  # same as cron +01:30
 
     assert cron.time() == ecf.TimeSeries(1, 30, True)
 
-def can_create_cron_that_runs_on_the_last_day_of_the_month():
+
+def test_create_cron_that_runs_on_the_last_day_of_the_month():
     cron = ecf.Cron()
     cron.set_last_day_of_the_month()
 
-    assert cron.last_day_of_the_month() == True
+    assert cron.last_day_of_the_month() is True
 
-def can_create_cron_that_runs_on_the_last_weekdays_of_the_month():
+
+def test_create_cron_that_runs_on_the_last_weekdays_of_the_month():
     cron = ecf.Cron()
     cron.set_last_week_days_of_the_month([3, 4, 5])
 
     assert cron.last_week_days_of_the_month == [3, 4, 5]
-
-
-if __name__ == "__main__":
-    Test.print_test_start(os.path.basename(__file__))
-
-    can_create_cron_from_default_parameters()
-    can_create_cron_that_runs_every_day_at_2pm()
-    can_create_cron_that_runs_every_30_minutes_between_0am_and_8pm_on_the_first_six_days_of_the_months_from_january_until_july()
-    can_create_cron_that_runs_relative_to_suite_start_time_or_task_requeue()
-    can_create_cron_based_on_time_series()
-    can_create_cron_with_defaults_and_set_time_series()
-    can_create_cron_that_runs_on_the_last_day_of_the_month()
-    can_create_cron_that_runs_on_the_last_weekdays_of_the_month()
-
-    print("All tests pass")
--- libs/pyext/test/py_u_TestMeter.py
+++ libs/pyext/test/py_u_TestMeter.py
@@ -8,14 +8,10 @@
 # nor does it submit to any jurisdiction.
 #
 
-import os
 import ecflow as ecf
-import itertools as it
-
-import ecflow_test_util as Test
 
 
-def can_create_meter_from_parameters():
+def test_create_meter_from_parameters():
     meter = ecf.Meter("name", 10, 100, 50)
     assert meter.name() == "name"
     assert meter.min() == 10
@@ -23,17 +19,9 @@ def can_create_meter_from_parameters():
     assert meter.color_change() == 50
 
 
-def can_create_meter_from_parameters_with_default_color_change():
+def test_create_meter_from_parameters_with_default_color_change():
     meter = ecf.Meter("name", 10, 100)
     assert meter.name() == "name"
     assert meter.min() == 10
     assert meter.max() == 100
     assert meter.color_change() == 100
-
-
-if __name__ == "__main__":
-    Test.print_test_start(os.path.basename(__file__))
-
-    can_create_meter_from_parameters()
-
-    print("All tests pass")
