e27109ee1190913f708d99590cb4408582e21cb0
Marcos Bento <Marcos.Bento@ecmwf.int>
2026-09-08 08:50:51 +0100

    Create thin wrapper around ecbuild_add_library
    Declare every library target with ecflow_add_library(), which mirrors
    ecflow_add_test(): it forwards all arguments to ecbuild_add_library()
    and adds a FORMAT option that registers the target sources with
    target_clangformat(). The test harness libraries are now gated on
    ENABLE_TESTS, alongside the tests that consume them.

--- cmake/BuildUtilities.cmake
+++ cmake/BuildUtilities.cmake
@@ -9,19 +9,102 @@
 #
 
 #
+# BuildUtilities.cmake provides thin wrappers around the ecbuild target
+# declaration functions, adding the FORMAT option so that a target and the
+# formatting of its sources are declared together.
+#
+
+#
+# _ecflow_format_target(<target> [CONDITION <condition>...])
+#
+# Register the sources of <target> with target_clangformat(), provided the
+# target has actually been created. The remaining arguments are forwarded
+# verbatim to target_clangformat().
+#
+# This is an implementation detail of the wrappers below and is not intended
+# to be called directly.
+#
+function(_ecflow_format_target target)
+  if (NOT target)
+    return()
+  endif()
+  if (NOT TARGET ${target})
+    return()
+  endif()
+
+  target_clangformat(${target} ${ARGN})
+endfunction()
+
+#
+# ecflow_add_library
+#
+# Declare a library target, as a thin wrapper around ecbuild_add_library().
+#
+#   ecflow_add_library(
+#     TARGET <target>
+#     [CONDITION <condition>...]
+#     [FORMAT]
+#     <ecbuild_add_library arguments>...
+#   )
+#
+# All other arguments are forwarded verbatim to ecbuild_add_library(); refer
+# to the documentation of that function for the full set of supported options.
+#
+# TARGET : required
+#   The name of the library target to create.
+#
+# CONDITION : optional
+#   A list of tokens forming a boolean expression, evaluated as a CMake if()
+#   condition. When the expression evaluates to FALSE, neither the library
+#   target nor the associated formatting target is created. The expression is
+#   forwarded both to ecbuild_add_library() and, when FORMAT is given, to
+#   target_clangformat(), so that both remain governed by the same condition.
+#
+# FORMAT : optional
+#   Register the sources of the library target with target_clangformat(),
+#   thereby creating the clangformat_<target> target and attaching it to the
+#   aggregate clangformat target. The formatting target is only created when
+#   the library target itself has been created.
+#
+function(ecflow_add_library)
+  set(options FORMAT)
+  set(single_value_args TARGET)
+  set(multi_value_args CONDITION)
+  cmake_parse_arguments(ARGS "${options}" "${single_value_args}" "${multi_value_args}" ${ARGN})
+
+  set(library_args ${ARGS_UNPARSED_ARGUMENTS})
+  if (DEFINED ARGS_TARGET)
+    list(PREPEND library_args TARGET ${ARGS_TARGET})
+  endif()
+  if (DEFINED ARGS_CONDITION)
+    list(APPEND library_args CONDITION ${ARGS_CONDITION})
+  endif()
+
+  ecbuild_add_library(${library_args})
+
+  if (ARGS_FORMAT)
+    set(format_args "")
+    if (DEFINED ARGS_CONDITION)
+      set(format_args CONDITION ${ARGS_CONDITION})
+    endif()
+    _ecflow_format_target("${ARGS_TARGET}" ${format_args})
+  endif()
+endfunction()
+
+#
 # ecflow_add_test
 #
 # Declare a test target, as a thin wrapper around ecbuild_add_test().
 #
 #   ecflow_add_test(
 #     TARGET <target>
-#     <ecbuild_add_test arguments>...
 #     [CONDITION <condition>...]
 #     [FORMAT]
+#     <ecbuild_add_test arguments>...
 #   )
 #
-# All arguments are forwarded verbatim to ecbuild_add_test(); refer to the
-# documentation of that function for the full set of supported options.
+# All other arguments are forwarded verbatim to ecbuild_add_test(); refer to
+# the documentation of that function for the full set of supported options.
 #
 # TARGET : required
 #   The name of the test target to create.
@@ -39,7 +122,6 @@
 #   clangformat target. The formatting target is only created when the test
 #   target itself has been created.
 #
-
 function(ecflow_add_test)
   set(options FORMAT)
   set(single_value_args TARGET)
@@ -56,13 +138,11 @@ function(ecflow_add_test)
 
   ecbuild_add_test(${test_args})
 
-  if (ARGS_FORMAT AND DEFINED ARGS_TARGET)
-    if (TARGET ${ARGS_TARGET})
-      set(format_args ${ARGS_TARGET})
-      if (DEFINED ARGS_CONDITION)
-        list(APPEND format_args CONDITION ${ARGS_CONDITION})
-      endif()
-      target_clangformat(${format_args})
+  if (ARGS_FORMAT)
+    set(format_args "")
+    if (DEFINED ARGS_CONDITION)
+      set(format_args CONDITION ${ARGS_CONDITION})
     endif()
+    _ecflow_format_target("${ARGS_TARGET}" ${format_args})
   endif()
 endfunction()
